f2f36c2fa6c1ba8db3928331d18b23d2889580ef
mochi-w650 <mochice.mls.ntl@gmail.com>
2020-09-10 00:41:16 +0900

    [FiX] fix about fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,27 @@
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
+
 function fish_user_key_bindings
-  bind \cr "peco_select_history (commandline -b)"
+    bind \cr "peco_select_history (commandline -b)"
+    bind \ce "peco-recentd"
 end
+
+echo "config run"
 
 # aliases 
 alias shutdown "sudo shutdown -h now"
@@ -22,3 +43,11 @@ if type "exa" > /dev/null 2>&1
 end 
 
 alias diff "git diff"
+
+# tmuxの自動起動
+set count (ps aux | grep tmux | grep -v grep | wc -l)
+if test $count -eq 0
+    command tmux
+else if test $count -eq 1
+    command tmux a
+end
