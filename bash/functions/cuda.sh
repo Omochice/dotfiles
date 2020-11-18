070b53d1a0d04ffb2eaf7501ce032a1a90ae15cb
Omochice-labsvr <h20ms419@hirosaki-u.ac.jp>
2020-11-18 18:09:09 +0900

    add: add registring cuda variables

---
+++ bash/functions/cuda.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash 
+
+if [ -d /usr/local/cuda -a -d /usr/local/cuda-10.2 ]; then
+    export PATH=/usr/local/cuda/bin:${PATH:+$PATH}
+    export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/cuda-10.2/lib64
+fi
