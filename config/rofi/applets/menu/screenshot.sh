4011052e52db63136f2cafed80058a23702656e4
mochi-home <mochi.mls.ntl@gmail.com>
2021-04-15 00:16:22 +0900

    update: update screenshot rofi

---
+++ config/rofi/applets/menu/screenshot.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+
+style="$($HOME/.config/rofi/applets/menu/style.sh)"
+
+dir="$HOME/.config/rofi/applets/menu/configs/$style"
+rofi_command="rofi -theme $dir/screenshot.rasi"
+
+# Error msg
+msg() {
+	rofi -theme "$HOME/.config/rofi/applets/styles/message.rasi" -e "$1"
+}
+
+# Options
+cancel="❎"
+movie="🎥"
+capture=""
+
+# Variable passed to rofi
+options="$cancel\n$movie\n$capture"
+
+chosen="$(echo -e "$options" | $rofi_command -p 'App : shots' -dmenu -selected-row 0)"
+case $chosen in
+    $cancel)
+        ;;
+    $movie)
+        if [ -x "$(command -v simplescreenrecorder)" ]; then 
+            command simplescreenrecorder 
+        else 
+            msg "simplescreenrecorder is not installed."
+        fi
+        ;;
+    $capture) 
+        if [ -x "$(command -v gyazo)" ]; then
+           gyazo 
+        else 
+            msg "gyazo is not installed."
+        fi 
+        ;;
+esac
