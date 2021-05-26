48ecb0ff664f689d2026671d682dfc29c732d525
mochi-MizLab <h20ms419@hirosaki-u.ac.jp>
2021-05-26 17:35:50 +0900

    move: move global environment veriables

--- bash/functions/global_envs.sh
+++ bash/functions/global_envs.sh
@@ -1,2 +1,5 @@
-EDITOR=vim
-PATH="$PATH":~/.anyenv/bin
+export STARSHIP_CONFIG=$HOME/.config/starship/config.toml
+export EDITOR=$(which vim)
+export TERMINAL=$(which alacritty)
+export BROWSER=$(which vivaldi-stable)
+export PATH="$PATH":~/.anyenv/bin
