da5ef563b17367c9baa3edf19f9c963f129b2513
mochi-MizLab <h20ms419@hirosaki-u.ac.jp>
2021-05-27 14:43:49 +0900

    update: remove peco function

--- bash/functions/function.sh
+++ bash/functions/function.sh
@@ -4,18 +4,18 @@
 function ex() {
     if [ -f $1 ]; then
         case $1 in
-        *.tar.bz2) tar xjf $1 ;;
-        *.tar.gz) tar xzf $1 ;;
-        *.bz2) bunzip2 $1 ;;
-        *.rar) unrar x $1 ;;
-        *.gz) gunzip $1 ;;
-        *.tar) tar xf $1 ;;
-        *.tbz2) tar xjf $1 ;;
-        *.tgz) tar xzf $1 ;;
-        *.zip) unzip $1 ;;
-        *.Z) uncompress $1 ;;
-        *.7z) 7z x $1 ;;
-        *) echo "'$1' cannot be extracted via ex()" ;;
+            *.tar.bz2) tar xjf $1 ;;
+            *.tar.gz) tar xzf $1 ;;
+            *.bz2) bunzip2 $1 ;;
+            *.rar) unrar x $1 ;;
+            *.gz) gunzip $1 ;;
+            *.tar) tar xf $1 ;;
+            *.tbz2) tar xjf $1 ;;
+            *.tgz) tar xzf $1 ;;
+            *.zip) unzip $1 ;;
+            *.Z) uncompress $1 ;;
+            *.7z) 7z x $1 ;;
+            *) echo "'$1' cannot be extracted via ex()" ;;
         esac
     else
         echo "'$1' is not a valid file"
@@ -29,12 +29,3 @@ function cdls() {
     fi
 }
 PROMPT_COMMAND="$PROMPT_COMMAND"$'\n'cdls
-
-# pecoの設定
-export HISTCONTROL=erasedups
-peco-select-history() {
-    declare l=$(HISTTIMEFORMAT= history | sed -e 's/^\s*[0-9]\+\s\+//' | peco --query "$READLINE_LINE")
-    READLINE_LINE="$l"
-    READLINE_POINT=${#l}
-}
-bind -x '"\C-r": peco-select-history'
