18e417176f4ffbca9751504ca49e0ad527651539
mochi-MizLab <mochice.mls.ntl@gmail.com>
2021-12-18 22:05:00 +0900

    :gear: Add template using argparse

---
+++ .vim/templates/python/snip-argparse.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+def parse_args() -> argparse.Namespace:
+    """Parse arguments
+
+    Args:
+
+    Returns:
+        argparse.Namespace:
+    """
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("--long", "-l", help="")
+    return parser.parse_args()
