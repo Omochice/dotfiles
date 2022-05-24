fb30da5e8a26207c2be5a3e72d42263299d79b94
Omochice <mochice.mls.ntl@gmail.com>
2022-05-24 09:43:43 +0900

    :+1: Add vue template

---
+++ .vim/templates/vue/base-component.vue
@@ -0,0 +1,23 @@
+<template>
+
+</template>
+
+
+<script>
+export default {
+  name: "SampleComponent",
+  data: () => {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+
+<style>
+
+</style>
