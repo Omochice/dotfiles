e8588af8db8fc89da02c1aae44d93e7d22f75283
Omochice <mochice.mls.ntl@gmail.com>
2022-06-18 00:56:36 +0900

    :hammer: update wezterm config

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,25 +2,25 @@ local wezterm = require("wezterm");
 local io = require("io")
 
 -- utils
-
 function merged(...)
     local results = {}
-    local tables = {...}
+    local tables = { ... }
     for i = 1, #tables do
-        results = _merged(results, tables[i])
+        _merged(results, tables[i])
     end
     return results
 end
 
 function _merged(t1, t2) -- from [https://github.com/yutkat/dotfiles/blob/3576916618fa7991de69682f628ec4832cf919c7/.config/wezterm/utils.lua]
-	for k, v in pairs(t2) do
-		if (type(v) == "table") and (type(t1[k] or false) == "table") then
-			merge_tables(t1[k], t2[k])
-		else
-			t1[k] = v
-		end
-	end
-	return t1
+    for k, v in pairs(t2) do
+        print(k)
+        if (type(v) == "table") and (type(t1[k] or false) == "table") then
+            _merged(t1[k], t2[k])
+        else
+            t1[k] = v
+        end
+    end
+    return t1
 end
 
 function basename(path)
@@ -37,14 +37,14 @@ function get_process_name(p)
 end
 
 function os.capture(cmd)
-  local f = assert(io.popen(cmd, 'r'))
-  local s = assert(f:read('*a'))
-  io.close(f)
-  local outputed = {}
-  for line in string.gmatch(s, "([^\n]*)\n?") do
-      table.insert(outputed, line)
-  end
-  return outputed
+    local f = assert(io.popen(cmd, 'r'))
+    local s = assert(f:read('*a'))
+    io.close(f)
+    local outputed = {}
+    for line in string.gmatch(s, "([^\n]*)\n?") do
+        table.insert(outputed, line)
+    end
+    return outputed
 end
 
 function is_opened_already()
@@ -56,8 +56,8 @@ end
 
 local fonts = {
     font = wezterm.font_with_fallback({
-        {family="Firge35Nerd Console", weight="Medium"},
-        {family="Consolas", weight="Medium"},
+        { family = "Firge35Nerd Console", weight = "Medium" },
+        { family = "Consolas", weight = "Medium" },
     }),
     font_size = 20,
 }
@@ -68,16 +68,16 @@ local keys = {
         { key = "c", mods = "ALT", action = wezterm.action({ SpawnTab = "DefaultDomain" }) },
         { key = "n", mods = "ALT", action = wezterm.action({ ActivateTabRelative = 1 }) },
         { key = "n", mods = "ALT|SHIFT", action = wezterm.action({ ActivateTabRelative = -1 }) },
-        { key = "-", mods = "ALT", action = wezterm.action({ SplitVertical = {domain = "CurrentPaneDomain"} }) },
+        { key = "-", mods = "ALT", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
         { key = "h", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
         { key = "j", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
         { key = "k", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
         { key = "l", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
-        { key = "LeftArrow", mods = "ALT", action = wezterm.action({ AdjustPaneSize = {"Left", 3} })},
-        { key = "DownArrow", mods = "ALT", action = wezterm.action({ AdjustPaneSize = {"Down", 3} })},
-        { key = "UpArrow", mods = "ALT", action = wezterm.action({ AdjustPaneSize = {"Up", 3} })},
-        { key = "RightArrow", mods = "ALT", action = wezterm.action({ AdjustPaneSize = {"Right", 3} })},
-        { key = "q", mods = "ALT|SHIFT", action = wezterm.action({ CloseCurrentPane = {confirm = false} })},
+        { key = "LeftArrow", mods = "ALT", action = wezterm.action({ AdjustPaneSize = { "Left", 3 } }) },
+        { key = "DownArrow", mods = "ALT", action = wezterm.action({ AdjustPaneSize = { "Down", 3 } }) },
+        { key = "UpArrow", mods = "ALT", action = wezterm.action({ AdjustPaneSize = { "Up", 3 } }) },
+        { key = "RightArrow", mods = "ALT", action = wezterm.action({ AdjustPaneSize = { "Right", 3 } }) },
+        { key = "q", mods = "ALT|SHIFT", action = wezterm.action({ CloseCurrentPane = { confirm = false } }) },
         { key = "q", mods = "ALT", action = "ActivateCopyMode" },
         { key = "[", mods = "ALT", action = "ActivateCopyMode" },
         { key = "r", mods = "ALT|SHIFT", action = "ReloadConfiguration" },
@@ -85,15 +85,15 @@ local keys = {
 }
 
 local additional_bindings = {}
-if os.capture("uname --kernel-name")[1] == "Darwin" then
+if wezterm.target_triple:find("darwin") then
     additional_bindings = {
-        { key = "mapped:¥", mods = "ALT", action = wezterm.action({ SplitHorizontal = {domain = "CurrentPaneDomain"} }) },
-        { key = "¥", action = wezterm.action({ SendString="\\" }) },
+        { key = "mapped:¥", mods = "ALT", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+        { key = "¥", action = wezterm.action({ SendString = "\\" }) },
         -- なぜかweztermだけyenとbackslashが反転するっぽい
     }
 else -- Linux
     additional_bindings = {
-        { key = "\\", mods = "ALT", action = wezterm.action({ SplitHorizontal = {domain = "CurrentPaneDomain"} }) },
+        { key = "\\", mods = "ALT", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
     }
 end
 for _, v in pairs(additional_bindings) do
@@ -133,37 +133,34 @@ local windows = {
     window_close_confirmation = "NeverPrompt",
 }
 
-local _sessions = {}
-for i = 1, 3 do
-    table.insert(_sessions, {name = "session" .. tostring(i)})
-end
-
 local domains = {
-    unix_domains = _sessions,
+    unix_domains = { name = "unix" },
+    default_gui_startup_args = { "connext", "unix" },
 }
-if not is_opened_already() then
-    domains.default_gui_startup_args = {"connect", "session1"}
-end
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-	local title = wezterm.truncate_right(get_process_name(tab.active_pane.title), max_width)
-	return {
-		{ Text = " " .. tab.tab_index + 1 .. ": " .. title .. " "},
-	}
+    local title = wezterm.truncate_right(get_process_name(tab.active_pane.title), max_width)
+    return {
+        { Text = " " .. tab.tab_index + 1 .. ": " .. title .. " " },
+    }
 end)
 
-others = {
+local others = {
     exit_behavior = "Close",
     use_ime = true,
 }
 
+if wezterm.target_triple:find("windows") then
+    others.default_prog = { "wsl.exe", "--distribution", "Ubuntu", "--exec", "bash", "-l" }
+    fonts.font_size = 14
+    domains = {}
+end
 
 return merged(
     keys,
     fonts,
     bars,
-    colors,
-    windows,
     domains,
+    windows,
     others
 )
