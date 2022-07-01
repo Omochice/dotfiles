cf2c9f331533cdf6fd06d94fa501d5aed8bb0039
Omochice <mochice.mls.ntl@gmail.com>
2022-07-01 18:44:15 +0900

    :hammer: Use expandHome instead

--- scripts/path-generator.ts
+++ scripts/path-generator.ts
@@ -7,18 +7,26 @@ import {
   Version,
 } from "https://raw.githubusercontent.com/stsysd/classopt/v0.1.2/mod.ts";
 import { parse } from "https://deno.land/std@0.140.0/encoding/toml.ts";
+import { expandHome } from "https://deno.land/x/expandhome@v0.0.5/mod.ts";
 
 type Shell = "bash" | "zsh" | "fish";
+
+function getShell(): Shell {
+  const tmp = (Deno.env.get("SHELL") ?? "bash").split("/");
+  return tmp[tmp.length - 1] as Shell;
+}
 
 interface Setting {
   paths: ExecutablePath[];
   environments: Environment[];
   aliases: Alias[];
+  sources: Source[];
 }
 
 interface ExecutablePath {
   path: string;
   if_executable?: string;
+  only?: Shell | Shell[];
 }
 
 interface Environment {
@@ -26,6 +34,7 @@ interface Environment {
   to: string;
   if_executable?: string;
   if_exists?: string;
+  only?: Shell | Shell[];
 }
 
 interface Alias {
@@ -33,6 +42,14 @@ interface Alias {
   to: string;
   if_executable?: string;
   if_exists?: string;
+  only?: Shell | Shell[];
+}
+
+interface Source {
+  path: string;
+  if_executable?: string;
+  if_exists?: string;
+  only?: Shell | Shell[];
 }
 
 async function loadToml(path: string): Promise<Setting> {
@@ -40,23 +57,29 @@ async function loadToml(path: string): Promise<Setting> {
   return (parse(await contents) as unknown as Setting);
 }
 
-function tildeToHome(path: string): string {
-  return path.replace(/^~/, "$HOME");
+function filterByShell<
+  T extends ExecutablePath | Environment | Alias | Source = ExecutablePath,
+>(
+  arr: T[],
+  shell: Shell,
+): T[] {
+  return arr.filter((e) =>
+    e.only == undefined || e.only == shell || e.only.includes(shell)
+  );
 }
 
 function generatePath(config: ExecutablePath, shell: Shell): string {
   if (shell == "fish") {
     return [
-      `if test -d ${config.path}`,
-      `\tset -x PATH ${tildeToHome(config.path)} \$PATH`,
-      `end`,
-    ].join("\n");
+      `test -d ${expandHome(config.path)}`,
+      `set -x PATH ${expandHome(config.path)}`,
+    ]
+      .join(" && ");
   } else {
     return [
-      `if [[ -d ${config.path} ]]; then`,
-      `\texport PATH=${tildeToHome(config.path)}:\$PATH`,
-      `fi`,
-    ].join("\n");
+      `[[ -d ${expandHome(config.path)} ]]`,
+      `export PATH=${expandHome(config.path)}:\$PATH`,
+    ].join(" && ");
   }
 }
 
@@ -67,7 +90,7 @@ function generateAlias(alias: Alias, shell: Shell): string {
       head.push(`command -v ${alias.if_executable} >/dev/null 2>&1`);
     }
     if (alias.if_exists) {
-      head.push(`test -e ${tildeToHome(alias.if_exists)}`);
+      head.push(`test -e ${expandHome(alias.if_exists)}`);
     }
     const body = `alias ${alias.to} "${alias.from}"`;
     return head.length > 0
@@ -78,7 +101,7 @@ function generateAlias(alias: Alias, shell: Shell): string {
       head.push(`command -v ${alias.if_executable} >/dev/null 2>&1`);
     }
     if (alias.if_exists) {
-      head.push(`[[ -e ${tildeToHome(alias.if_exists)} ]]`);
+      head.push(`[[ -e ${expandHome(alias.if_exists)} ]]`);
     }
     const body = `alias ${alias.to}=${alias.from}`;
     return head.length > 0
@@ -89,14 +112,16 @@ function generateAlias(alias: Alias, shell: Shell): string {
 
 function generateEnviroment(environment: Environment, shell: Shell): string {
   const head = [];
+  const from = expandHome(environment.from);
+  const to = expandHome(environment.to);
   if (shell == "fish") {
     if (environment.if_executable) {
       head.push(`command -v ${environment.if_executable} >/dev/null 2>&1`);
     }
     if (environment.if_exists) {
-      head.push(`test -e ${tildeToHome(environment.if_exists)}`);
+      head.push(`test -e ${expandHome(environment.if_exists)}`);
     }
-    const body = `set ${environment.to} ${environment.from}`;
+    const body = `set ${to} ${from}`;
     return head.length > 0
       ? [`if ${head.join(" && ")}`, `\t${body}`, `end`].join("\n")
       : body;
@@ -105,13 +130,26 @@ function generateEnviroment(environment: Environment, shell: Shell): string {
       head.push(`command -v ${environment.if_executable} >/dev/null 2>&1`);
     }
     if (environment.if_exists) {
-      head.push(`[[ -e ${tildeToHome(environment.if_exists)} ]]`);
+      head.push(`[[ -e ${expandHome(environment.if_exists)} ]]`);
     }
-    const body = `export ${environment.to}=${environment.from}`;
-    return head.length > 0
-      ? [`if ${head.join(" && ")}; then`, `\t${body}`, `fi`].join("\n")
-      : body;
+    head.push(`export ${to}=${from}`);
+    return head.join(" && ");
   }
+}
+
+function generateSource(source: Source, _shell: Shell): string {
+  const body = [];
+  if (source.if_executable) {
+    body.push(`command -v ${source.if_executable} >/dev/null 2>&1`);
+  }
+  if (source.if_exists) {
+    body.push(`[[ -e ${expandHome(source.if_exists)} ]]`);
+  }
+  body.push(
+    `[[ -f ${expandHome(source.path)} ]]`,
+    `source ${expandHome(source.path)}`,
+  );
+  return body.join(" && ");
 }
 
 @Help("Path file generator for me")
@@ -120,16 +158,17 @@ class Program extends Command {
   @Arg({ about: "path to config file" })
   config!: string;
 
-  @Opt({ about: `Format shell type ${Deno.env.get("shell")}` })
-  shell: Shell = Deno.env.get("shell") as Shell;
+  @Opt({ about: `Format shell type. default: ${getShell()}` })
+  shell: Shell = getShell();
 
   @Flag({ about: "enable debug mode" })
   debug = false;
 
   async execute() {
+    console.log(this.shell);
     const tomlData = await loadToml(this.config);
     const paths: string[] = [];
-    for (const p of tomlData.paths) {
+    for (const p of filterByShell(tomlData.paths ?? [], this.shell)) {
       if (this.debug) {
         console.error(p);
         console.error(generatePath(p, this.shell));
@@ -138,7 +177,7 @@ class Program extends Command {
     }
 
     const aliases: string[] = [];
-    for (const al of tomlData.aliases) {
+    for (const al of filterByShell(tomlData.aliases ?? [], this.shell)) {
       if (this.debug) {
         console.error(al);
         console.error(generateAlias(al, this.shell));
@@ -147,7 +186,7 @@ class Program extends Command {
     }
 
     const envs: string[] = [];
-    for (const env of tomlData.environments) {
+    for (const env of filterByShell(tomlData.environments ?? [], this.shell)) {
       if (this.debug) {
         console.error(env);
         console.error(generateEnviroment(env, this.shell));
@@ -155,11 +194,23 @@ class Program extends Command {
       envs.push(generateEnviroment(env, this.shell));
     }
 
-    console.log([
-      paths.join("\n"),
-      aliases.join("\n"),
-      envs.join("\n"),
-    ].join("\n\n"));
+    const sources: string[] = [];
+    for (const source of filterByShell(tomlData.sources ?? [], this.shell)) {
+      if (this.debug) {
+        console.error(source);
+        console.error(generateSource(source, this.shell));
+      }
+      sources.push(generateSource(source, this.shell));
+    }
+
+    console.log(
+      [
+        paths.join("\n"),
+        aliases.join("\n"),
+        envs.join("\n"),
+        sources.join("\n"),
+      ].filter((e) => e.length > 0).join("\n\n"),
+    );
   }
 }
 
