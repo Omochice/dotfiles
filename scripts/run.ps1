399c2331201461cc52a0a185aebcf4bb5f1ad893
Omochice <mochice.mls.ntl@gmail.com>
2022-07-19 02:06:24 +0900

    :hammer: Update setting for windows

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -7,8 +7,17 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 $DotDir = ([System.IO.FileInfo]$PSScriptRoot).Directory.FullName
 $UserProfile = $env:USERPROFILE
 
-# show extention
+# setting by registry {{{
+## show extention
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /t REG_DWORD /d "0" /f
+
+## Disable sound by notification
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Notitications\Settings" /v "NOC_GLOBAL_SETTING_ALLOW_NOTIFICATION_SOUND" /t REG_DWORD /d "0" /f
+
+## Disable hints
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SoftLandingEnabled" /t REG_DWORD /d "0" /f
+
+# }}}
 
 ## wezterm
 if ($null -eq (Get-Command "wezterm.exe" -ErrorAction SilentlyContinue)) {
@@ -18,9 +27,32 @@ Get-ChildItem -Path (Join-Path -Path $DotDir -ChildPath config/wezterm) | ForEac
     New-Item -ItemType SymbolicLink -Path (Join-Path -Path $env:ProgramW6432 -ChildPath $_.Name) -Target $_.FullName -Force
 }
 
+## Browsers {{{
+
+## Vivaldi
+if ($null -eq (Get-Command (Join-Path -Path $UserProfile -ChildPath "AppData/Local/Vivaldi/application/vivaldi.exe") -ErrorAction SilentlyContinue)) {
+    winget install VivaldiTechnologies.Vivaldi
+}
+
+## Chrome
+if ($null -eq (Get-Command (Join-Path -Path $env:ProgramW6432 -ChildPath "Google/Chrome/application/chrome.exe") -ErrorAction SilentlyContinue)) {
+    winget install VivaldiTechnologies.Vivaldi
+}
+
+## Firefox
+if ($null -eq (Get-Command (Join-Path -Path $env:ProgramW6432 -ChildPath "Mozilla Firefox/firefox.exe") -ErrorAction SilentlyContinue)) {
+    winget install VivaldiTechnologies.Vivaldi
+}
+## }}}
+
+## vscode
+if ($null -eq (Get-Command "code")) {
+    winget install Microsoft.VisutalStudioCode
+}
+
 ## powertoys
-if ($null -eq (Get-Command (Join-Path -Path $env:ProgramW6432 -ChildPath "PowerToys/PowerToys.exe") -ErrorAction SilentlyContinue)) {
-    winget install Microsoft.PowerToys
+if ($null -eq (Get-Command (Join-Path -Path $env:ProgramW6432 -ChildPath "Google/Chrome/application/chrome.exe") -ErrorAction SilentlyContinue)) {
+    winget install Google.Chrome
 }
 Get-ChildItem -Path (Join-Path -Path $DotDir -ChildPath "win-config/PowerToys") -Recurse -File | ForEach-Object {
     New-Item `
