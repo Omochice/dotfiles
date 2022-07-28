9c33d196af911267af44ab9305e51868cdb56102
Omochice <mochice.mls.ntl@gmail.com>
2022-07-28 18:42:42 +0900

    :muscle: Use `-EditMode Emacs` instead

--- config/powershell/profile.ps1
+++ config/powershell/profile.ps1
@@ -1,16 +1,6 @@
 # from https://zenn.dev/t4t5u0/articles/windows-develop-setup
 Import-Module PSReadLine
 
-Set-PSReadLineKeyHandler -Key "Ctrl+d" -Function DeleteCharOrExit
-Set-PSReadLineKeyHandler -Key "Ctrl+u" -Function BackwardDeleteLine
-Set-PSReadLineKeyHandler -Key "Ctrl+k" -Function ForwardDeleteLine
-Set-PSReadLineKeyHandler -Key "Ctrl+a" -Function BeginningOfLine
-Set-PSReadLineKeyHandler -Key "Ctrl+e" -Function EndOfLine
-Set-PSReadLineKeyHandler -Key "Ctrl+f" -Function ForwardChar
-Set-PSReadLineKeyHandler -Key "Ctrl+b" -Function BackwardChar
-Set-PSReadLineKeyHandler -Key "Alt+f" -Function NextWord
-Set-PSReadLineKeyHandler -Key "Alt+b" -Function BackwardWord
-Set-PSReadLineKeyHandler -Key "Ctrl+p" -Function PreviousHistory
-Set-PSReadLineKeyHandler -Key "Ctrl+n" -Function NextHistory
+Set-PSReadLineOption -EditMode Emacs
 
 Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
