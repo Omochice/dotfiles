607ba7459c967a2720885da3b3bbaa33dd6873ff
Omochice <mochice.mls.ntl@gmail.com>
2022-08-02 18:32:30 +0900

    :gear: update wezterm setting

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -80,7 +80,19 @@ local keys = {
         { key = "q", mods = "ALT", action = "ActivateCopyMode" },
         { key = "[", mods = "ALT", action = "ActivateCopyMode" },
         { key = "r", mods = "ALT|SHIFT", action = "ReloadConfiguration" },
-    }
+        { key = "1", mods = "ALT", action = wezterm.action({ ActivateTab = 0 }) },
+        { key = "2", mods = "ALT", action = wezterm.action({ ActivateTab = 1 }) },
+        { key = "3", mods = "ALT", action = wezterm.action({ ActivateTab = 2 }) },
+        { key = "4", mods = "ALT", action = wezterm.action({ ActivateTab = 3 }) },
+        { key = "5", mods = "ALT", action = wezterm.action({ ActivateTab = 4 }) },
+        { key = "6", mods = "ALT", action = wezterm.action({ ActivateTab = 5 }) },
+        { key = "7", mods = "ALT", action = wezterm.action({ ActivateTab = 6 }) },
+        { key = "8", mods = "ALT", action = wezterm.action({ ActivateTab = 7 }) },
+        { key = "9", mods = "ALT", action = wezterm.action({ ActivateTab = 8 }) },
+        { key = "Enter", mods = "ALT", action = "QuickSelect" },
+        { key = "/", mods = "ALT", action = act.Search("CurrentSelectionOrEmptyString") },
+    },
+    -- TODO: copy-modeのときにC-u/dを使えるようにする
 }
 
 local additional_bindings = {}
