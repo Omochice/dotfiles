243696652f878399d671e8e1d9f894497e785fdd
Omochice <mochice.mls.ntl@gmail.com>
2022-08-26 02:17:31 +0900

    :muscle: make windowstyle specifiable

--- scripts/functions.ps1
+++ scripts/functions.ps1
@@ -1,13 +1,19 @@
 function Create-ShortCut {
-    param([string]$Source, [string[]]$Arguments, [string]$Destination)
+    param([string]$Source, [string[]]$Arguments, [string]$Destination, [int]$WindowStyle)
     $WScriptShell = New-Object -ComObject WScript.Shell
     $executableName = Split-Path $Source -Leaf
     $name = $executableName.SubString(0, $executableName.LastIndexOf("."))
     $dst = Join-Path -Path (Resolve-Path -Path $Destination) -ChildPath ($name + ".lnk")
     $Shortcut = $WScriptShell.CreateShortcut($dst)
     $Shortcut.Targetpath = $Source
-    if ($Arguments.count -gt 0){
+    if ($Arguments.count -gt 0) {
         $Shortcut.Arguments = $Arguments -join " "
+    }
+    if ($WindowStyle -gt 0) {
+        # 1: normal
+        # 3: maximize
+        # 7: minimize
+        $Shortcut.WindowStyle = $WindowStyle
     }
     $Shortcut.Save()
 
