8b1757e699232a51faba7fd404c64ad021123bf3
mochi-MizLab <mochice.mls.ntl@gmail.com>
2022-11-09 00:46:09 +0900

    :+1: Add nvim build shell script

---
+++ scripts/install-nvim.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# original is from Shougo/shougo-s-github/install-nvim
+
+make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=~/.local/nvim install
+
+# Remove default plugins
+if [ -d ~/.local/nvim/share/nvim/runtime/plugin ]; then
+    rm ~/.local/nvim/share/nvim/runtime/plugin/gzip.vim
+    rm ~/.local/nvim/share/nvim/runtime/plugin/health.vim
+    rm ~/.local/nvim/share/nvim/runtime/plugin/matchit.vim
+    rm ~/.local/nvim/share/nvim/runtime/plugin/matchparen.vim
+    rm ~/.local/nvim/share/nvim/runtime/plugin/netrwPlugin.vim
+    rm ~/.local/nvim/share/nvim/runtime/plugin/shada.vim
+    rm ~/.local/nvim/share/nvim/runtime/plugin/spellfile.vim
+    rm ~/.local/nvim/share/nvim/runtime/plugin/tarPlugin.vim
+    rm ~/.local/nvim/share/nvim/runtime/plugin/tohtml.vim
+    rm ~/.local/nvim/share/nvim/runtime/plugin/tutor.vim
+    rm ~/.local/nvim/share/nvim/runtime/plugin/zipPlugin.vim
+fi
+if [ -f ~/.local/nvim/share/nvim/runtime/ftplugin.vim ]; then
+    rm ~/.local/nvim/share/nvim/runtime/ftplugin.vim
+fi
+if [ -d /usr/share/vim/vimfiles/plugin ]; then
+    rm -Rf /usr/share/vim/vimfiles/plugin
+fi
+if [ -f /etc/xdg/nvim/sysinit.vim ]; then
+    rm /etc/xdg/nvim/sysinit.vim
+fi
+if [ -f /usr/share/nvim/archlinux.vim ]; then
+    rm /usr/share/nvim/archlinux.vim
+fi
