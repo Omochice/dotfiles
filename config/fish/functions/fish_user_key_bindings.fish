f9a8425f38cd0dfe52c8b6946a4959d23aae0bae
Omochice <mochice.mls.ntl@gmail.com>
2022-12-13 18:54:19 +0900

    :muscle: C-q to find file with fzf

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -2,6 +2,7 @@ function fish_user_key_bindings
   if type "emptyls" >/dev/null 2>&1
     bind \r emptyls
   end
+  bind \cq fzf-nvim
   bind \cr fzf_history
   bind \cg __lazygit
   bind \cT fzf-emoji
