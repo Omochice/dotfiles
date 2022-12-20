b60b941e0895b289ca57be1b1cecfa742a4c3846
Omochice <mochice.mls.ntl@gmail.com>
2022-12-20 09:55:29 +0900

    :muscle: update fish func

--- config/fish/functions/__lazygit.fish
+++ config/fish/functions/__lazygit.fish
@@ -1,7 +1,8 @@
 function __lazygit
-    set -l cmd (commandline)
+    set --local cmd (commandline)
     if test -z "$cmd"
         lazygit
     end
     fish_default_mode_prompt
+    commandline --function repaint
 end
