ca923ac313c2bcd068a4bb5f2d737b2fa196a079
Omochice <mochice.mls.ntl@gmail.com>
2023-03-13 02:29:44 +0900

    :muscle: refactor sketchybar settings

--- config/sketchybar/scripts/speedtest.sh
+++ config/sketchybar/scripts/speedtest.sh
@@ -2,8 +2,8 @@
 
 speed=$(networkQuality -c)
 
-download=$(echo $speed | jq 'if .dl_throughput then .["dl_throughput"] / 1024 / 1024 else 0 end' | sed -e "s/\(\.[0-9]\{0,3\}\)[0-9]*/\1/g")
-upload=$(echo $speed | jq 'if .ul_throughput then .["ul_throughput"] / 1024 / 1024 else 0 end' | sed -e "s/\(\.[0-9]\{0,3\}\)[0-9]*/\1/g")
+download=$(echo $speed | jq 'if .dl_throughput then .["dl_throughput"] / 1024 / 1024 else 0 end' | sed -e "s/\.[0-9]\{0,3\}[0-9]*//g")
+upload=$(echo $speed | jq 'if .ul_throughput then .["ul_throughput"] / 1024 / 1024 else 0 end' | sed -e "s/\.[0-9]\{0,3\}[0-9]*//g")
 
 if [[ "$download" = "null" ]] || [[ "$upload" = "null" ]] ; then
     exit 1
