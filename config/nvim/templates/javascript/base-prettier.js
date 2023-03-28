1d80b9cd88adffb446c9f9b20b16e3125d7d27ea
Omochice <mochice.mls.ntl@gmail.com>
2023-03-28 22:34:17 +0900

    :+1: Add prettier template

---
+++ config/nvim/templates/javascript/base-prettier.js
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: "lf",
+  jsxSingleQuote: false,
+  printWidth: 80,
+  semi: true,
+  singleAttributePerLine: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+};
