62ba9ce502e130d69091d4d8952b6320daec218f
Omochice <mochice.mls.ntl@gmail.com>
2023-04-25 10:18:00 +0900

    :muscle: Update install script

--- scripts/install-nvim.sh
+++ scripts/install-nvim.sh
@@ -1,25 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 # original is from Shougo/shougo-s-github/install-nvim
 
+install_to="${HOME}/.local/nvim"
+
 make clean
-make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=~/.local/nvim install
+make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=$install_to install
 
 # Remove default plugins
-if [ -d ~/.local/nvim/share/nvim/runtime/plugin ]; then
-    [ -f ~/.local/nvim/share/nvim/runtime/plugin/gzip.vim ] && rm ~/.local/nvim/share/nvim/runtime/plugin/gzip.vim
-    [ -f ~/.local/nvim/share/nvim/runtime/plugin/health.vim ] && rm ~/.local/nvim/share/nvim/runtime/plugin/health.vim
-    [ -f ~/.local/nvim/share/nvim/runtime/plugin/matchit.vim ] && rm ~/.local/nvim/share/nvim/runtime/plugin/matchit.vim
-    [ -f ~/.local/nvim/share/nvim/runtime/plugin/matchparen.vim ] && rm ~/.local/nvim/share/nvim/runtime/plugin/matchparen.vim
-    [ -f ~/.local/nvim/share/nvim/runtime/plugin/netrwPlugin.vim ] && rm ~/.local/nvim/share/nvim/runtime/plugin/netrwPlugin.vim
-    [ -f ~/.local/nvim/share/nvim/runtime/plugin/shada.vim ] && rm ~/.local/nvim/share/nvim/runtime/plugin/shada.vim
-    [ -f ~/.local/nvim/share/nvim/runtime/plugin/spellfile.vim ] && rm ~/.local/nvim/share/nvim/runtime/plugin/spellfile.vim
-    [ -f ~/.local/nvim/share/nvim/runtime/plugin/tarPlugin.vim ] && rm ~/.local/nvim/share/nvim/runtime/plugin/tarPlugin.vim
-    [ -f ~/.local/nvim/share/nvim/runtime/plugin/tohtml.vim ] && rm ~/.local/nvim/share/nvim/runtime/plugin/tohtml.vim
-    [ -f ~/.local/nvim/share/nvim/runtime/plugin/tutor.vim ] && rm ~/.local/nvim/share/nvim/runtime/plugin/tutor.vim
-    [ -f ~/.local/nvim/share/nvim/runtime/plugin/zipPlugin.vim] && rm ~/.local/nvim/share/nvim/runtime/plugin/zipPlugin.vim
+if [ -d "${install_to}/share/nvim/runtime/plugin" ]; then
+    [ -f "${install_to}/share/nvim/runtime/plugin/gzip.vim" ] && rm "${install_to}/share/nvim/runtime/plugin/gzip.vim"
+    [ -f "${install_to}/share/nvim/runtime/plugin/health.vim" ] && rm "${install_to}/share/nvim/runtime/plugin/health.vim"
+    [ -f "${install_to}/share/nvim/runtime/plugin/matchit.vim" ] && rm "${install_to}/share/nvim/runtime/plugin/matchit.vim"
+    [ -f "${install_to}/share/nvim/runtime/plugin/matchparen.vim" ] && rm "${install_to}/share/nvim/runtime/plugin/matchparen.vim"
+    [ -f "${install_to}/share/nvim/runtime/plugin/netrwPlugin.vim" ] && rm "${install_to}/share/nvim/runtime/plugin/netrwPlugin.vim"
+    [ -f "${install_to}/share/nvim/runtime/plugin/shada.vim" ] && rm "${install_to}/share/nvim/runtime/plugin/shada.vim"
+    [ -f "${install_to}/share/nvim/runtime/plugin/spellfile.vim" ] && rm "${install_to}/share/nvim/runtime/plugin/spellfile.vim"
+    [ -f "${install_to}/share/nvim/runtime/plugin/tarPlugin.vim" ] && rm "${install_to}/share/nvim/runtime/plugin/tarPlugin.vim"
+    [ -f "${install_to}/share/nvim/runtime/plugin/tohtml.vim" ] && rm "${install_to}/share/nvim/runtime/plugin/tohtml.vim"
+    [ -f "${install_to}/share/nvim/runtime/plugin/tutor.vim" ] && rm "${install_to}/share/nvim/runtime/plugin/tutor.vim"
+    [ -f "${install_to}/share/nvim/runtime/plugin/zipPlugin.vim" ] && rm "${install_to}/share/nvim/runtime/plugin/zipPlugin.vim"
 fi
-if [ -f ~/.local/nvim/share/nvim/runtime/ftplugin.vim ]; then
-    rm ~/.local/nvim/share/nvim/runtime/ftplugin.vim
+if [ -f "${install_to}/share/nvim/runtime/ftplugin.vim" ]; then
+    rm "${install_to}/share/nvim/runtime/ftplugin.vim"
 fi
 if [ -d /usr/share/vim/vimfiles/plugin ]; then
     rm -Rf /usr/share/vim/vimfiles/plugin
