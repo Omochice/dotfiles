b910a810f77242171b4a6a7600059a2dac312354
Omochice <mochice.mls.ntl@gmail.com>
2023-05-14 16:31:44 +0900

    :muscle: use deno script instead
    The case when "brew" is not set in path,
    `chsh -s /bin/fish` show error.

---
+++ scripts/tasks/chsh-to-fish.ts
@@ -0,0 +1,28 @@
+import $ from "https://deno.land/x/dax@0.31.1/mod.ts";
+import { getBrewPath } from "./install-brew.ts";
+import { err, ok, Result } from "npm:neverthrow@6.0.0";
+
+function getFishPath(): Result<string, Error> {
+  const brewResult = getBrewPath();
+  if (brewResult.isErr()) {
+    return err(brewResult.error);
+  }
+  return ok($.path.join($.path.dirname(brewResult.value), "fish"));
+}
+
+async function chsh(): Promise<Result<void, Error>> {
+  const fishPathResult = getFishPath();
+  if (fishPathResult.isErr()) {
+    return err(fishPathResult.error);
+  }
+  await $`sudo chsh --shell ${fishPathResult.value}`;
+  return ok(undefined);
+}
+
+if (import.meta.main) {
+  const result = await chsh();
+  if (result.isErr()) {
+    console.error(result.error);
+    Deno.exit(2);
+  }
+}
