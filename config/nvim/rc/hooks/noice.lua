bd1b0fd8398eb7266add8fdde1c25be7b0f8f8ff
Omochice <mochice.mls.ntl@gmail.com>
2023-05-15 01:32:11 +0900

    :package: use hooks_file on noice

---
+++ config/nvim/rc/hooks/noice.lua
@@ -0,0 +1,29 @@
+-- lua_add {{{
+vim.opt.cmdheight = 0
+-- }}}
+
+-- lua_source {{{
+require("noice").setup({
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = true, -- use a classic bottom cmdline for search
+    command_palette = true, -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false, -- add a border to hover docs and signature help
+  },
+  messages = {
+    enabled = true,
+    view = "mini",
+    view_error = "mini",
+    view_warn = "mini",
+    view_history = "messages",
+    view_search = "virtualtext",
+  },
+  lsp = {
+    hover = {
+      enabled = false,
+    }
+  }
+})
+-- }}}
