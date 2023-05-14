f65aeea955e761b5af48ead44cd8f85ffd040463
Omochice <mochice.mls.ntl@gmail.com>
2023-05-14 18:03:29 +0900

    :+1: Add /etc/shells update task

---
+++ scripts/tasks/show-fishpath.ts
@@ -0,0 +1,28 @@
+import $ from "https://deno.land/x/dax@0.31.1/mod.ts";
+import { getBrewPath } from "./install-brew.ts";
+import { err, ok, Result } from "npm:neverthrow@6.0.0";
+
+function getFishPath(): Result<string, Error> {
+  const result = getBrewPath();
+  if (result.isErr()) {
+    return err(result.error);
+  }
+
+  const fishPath = $.path(result.value).parent()?.join("fish");
+  if (fishPath === undefined || !fishPath.existsSync()) {
+    return err(
+      new Error(`Path of fish is missing...: ${fishPath?.toString()}`),
+    );
+  }
+
+  return ok(fishPath.toString());
+}
+
+if (import.meta.main) {
+  const result = getFishPath();
+  if (result.isErr()) {
+    console.error(result.error);
+    Deno.exit(2);
+  }
+  console.log(result.value);
+}
