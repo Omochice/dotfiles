737ce8c20daecf22f50d3bf916462250967b9201
Omochice <mochice.mls.ntl@gmail.com>
2023-06-05 22:06:06 +0900

    :bug: fix uname
    bsd `uname` havent --kernel-release option

--- scripts/tasks/mod.ts
+++ scripts/tasks/mod.ts
@@ -1,6 +1,6 @@
 import $ from "https://deno.land/x/dax@0.31.1/mod.ts";
 
-export const ISWSL = /microsoft/.test(await $`uname --kernel-release`.text());
+export const ISWSL = Deno.build.os === "linux" && /microsoft/.test(await $`uname --kernel-release`.text());
 
 export const DOTDIR = $.path.dirname(
   $.path.dirname(
