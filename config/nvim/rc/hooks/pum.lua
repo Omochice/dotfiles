a12ce528ddb91609774a2f80887c54848cd57855
Omochice <mochice.mls.ntl@gmail.com>
2023-06-09 16:40:06 +0900

    :muscle: use hooks file instead

--- config/nvim/rc/hooks/pum.lua
+++ config/nvim/rc/hooks/pum.lua
@@ -3,9 +3,16 @@ local vimx = require("artemis")
 vimx.keymap.set(
   "i",
   [[<C-y>]],
-  function()
-    vimx.fn.pum.map.confirm()
-  end
+  "",
+  {
+    expr = true,
+    callback = function()
+      if vimx.fn.pum.visible() then
+        vimx.fn.pum.map.confirm()
+      end
+      return [[<C-y>]]
+    end
+  }
 )
 vimx.keymap.set(
   "i",
@@ -14,15 +21,29 @@ vimx.keymap.set(
   {
     expr = true,
     callback = function()
-      if vimx.fn.pum.visible() == 1 then
+      if vimx.fn.pum.visible() then
         vimx.fn.pum.map.insert_relative(1)
       end
-      return [[<C-y]]
+      return [[<C-n>]]
     end
   }
 )
 vimx.keymap.set(
   "i",
-  [[<C-p>]]
+  [[<C-p>]],
+  "",
+  {
+    expr = true,
+    callback = function()
+      if vimx.fn.pum.visible() then
+        vimx.fn.pum.map.insert_relative(-1)
+      end
+      return [[<C-p>]]
+    end
+  }
 )
+-- }}}
+
+-- lua_source {{{
+require("artemis").fn.pum.set_option("auto_select", true)
 -- }}}
