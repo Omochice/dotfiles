6589958d2ebd474b40400fc54395aacf952240da
Omochice <mochice.mls.ntl@gmail.com>
2023-06-11 02:48:42 +0900

    :muscle: use typescript config instead

--- config/nvim/rc/hooks/ddc.lua
+++ config/nvim/rc/hooks/ddc.lua
@@ -1,15 +1,5 @@
 -- lua_add {{{
 local vimx = require("artemis")
-vimx.g.ddc_source_options = {
-  _ = {
-    matchers = { "matcher_fuzzy" },
-    sorters = { "sorter_fuzzy" },
-    converters = { "converter_remove_overlap", "converter_truncate_abbr", "converter_fuzzy" },
-    minAutoCompleteLength = 2,
-    ignoreCase = true,
-  }
-}
-
 local sources = {
   snippet = { "vsnip" },
   lsp = vimx.fn.has("nvim") and { "nvim-lsp" } or { "vim-lsp" },
@@ -33,39 +23,9 @@ vimx.keymap.set(
 --     vimx.fn.ddc.map.manual_complete(sources.lsp)
 --   end
 -- )
-if vimx.fn.dein.tap("vim-vsnip-integ") == 1 then
-  vimx.g.ddc_source_options.vsnip = { mark = "[Snp]" }
-end
 -- }}}
 
 -- lua_source {{{
 local vimx = require("artemis")
-vimx.fn.ddc.custom.patch_global(
-  "sources",
-  { "vsnip", "nvim-lsp", "around", "buffer", "rg", }
-)
-
--- TODO: なんかよくわからんけどluaだといけない
--- vim.print(vimx.cast(vimx.g.ddc_source_options))
--- vimx.fn.ddc.custom.patch_global(
---   "sourceOptions",
---   vimx.g.ddc_source_options
--- )
-vimx.cmd([[call ddc#custom#patch_global('sourceOptions', g:ddc_source_options)]])
-
-vimx.fn.ddc.custom.patch_filetype(
-  { "toml", "vim" },
-  "sources",
-  { "vsnip", "necovim", "nvim-lsp", "around", "buffer", "rg" }
-)
-
-vimx.fn.ddc.custom.patch_filetype(
-  "markdown",
-  "sources",
-  { "vsnip", "file", "around", "buffer", "rg", }
-)
-
-vimx.fn.ddc.custom.patch_global("ui", "pum")
-vimx.fn.ddc.custom.patch_global("backspaceCompletion", true)
-vimx.fn.ddc.enable()
+vimx.fn.ddc.custom.load_config(vimx.fn.expand("$DEIN_RC_DIR/ts/ddc.ts"))
 -- }}}
