4df0f47ce25ebf1eb1e114cfbfded9262ead09e4
Omochice <mochice.mls.ntl@gmail.com>
2023-06-20 19:41:39 +0900

    :muscle: use ddu instead

--- config/nvim/rc/hooks/lspsaga.lua
+++ config/nvim/rc/hooks/lspsaga.lua
@@ -1,15 +1,3 @@
--- lua_add {{{
-local group = vim.api.nvim_create_augroup("vimrc#lspsaga", {
-  clear = true
-})
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(_)
-    vim.keymap.set("n", "<Space>a", [[<Cmd>Lspsaga code_action<CR>]], { buffer = true })
-  end,
-  group = group,
-})
--- }}}
-
 -- lua_source {{{
 require("lspsaga").setup({})
 -- }}}
