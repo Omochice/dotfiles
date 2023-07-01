819768b4de9b44729e7f3d8b5cf92c4509796b03
Omochice <mochice.mls.ntl@gmail.com>
2023-07-02 05:44:08 +0900

    :muscle: use hooks-file instead

---
+++ config/nvim/rc/hooks/vsnip.lua
@@ -0,0 +1,71 @@
+-- lua_add {{{
+local vimx = require("artemis")
+vimx.g.vsnip_snippet_dir = vimx.g.config_dir .. "/snippets"
+vimx.g.vsnip_filetypes = {
+  plaintex = { "tex" },
+  objc = { "c", "objc" },
+  vue = { "javascript", "typescript" },
+}
+-- }}}
+
+-- lua_source {{{
+local vimx = require("artemis")
+vimx.keymap.set(
+  "i",
+  [[<Tab>]],
+  "",
+  {
+    expr = true,
+    callback = function()
+      if vimx.fn.vsnip.jumpable(1) then
+        return [[<Plug>(vsnip-jump-next)]]
+      end
+      vimx.fn.lexima.expend([[<LT>TAB>]], "i")
+      return ""
+    end,
+  }
+)
+vimx.keymap.set(
+  "s",
+  [[<Tab>]],
+  "",
+  {
+    expr = true,
+    callback = function()
+      if vimx.fn.vsnip.jumpable(1) then
+        return [[<Plug>(vsnip-jump-next)]]
+      end
+      return [[<Tab>]]
+    end
+  }
+)
+vimx.keymap.set(
+  "i",
+  "[[<S-Tab>]]",
+  "",
+  {
+    expr = true,
+    callback = function()
+      if vimx.fn.vsnip.jumpable(-1) then
+        return [[<Plug>(vsnip-jump-prev)]]
+      end
+      vimx.fn.lexima.expend([[<LT>S-TAB>]], "i")
+      return ""
+    end
+  }
+)
+vimx.keymap.set(
+  "s",
+  [[<S-Tab>]],
+  "",
+  {
+    expr = true,
+    callback = function()
+      if vimx.fn.vsnip.jumpable(1) then
+        return [[<Plug>(vsnip-jump-prev)]]
+      end
+      return [[<S-Tab>]]
+    end
+  }
+)
+-- }}}
