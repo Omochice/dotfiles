c0b48a4c8740d674d40084cdcc1fd9a6e3f2c52d
Omochice <mochice.mls.ntl@gmail.com>
2023-07-08 20:02:10 +0900

    :+1: Add cliffy template

---
+++ config/nvim/templates/typescript/base-cliffy.ts
@@ -0,0 +1,18 @@
+import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.2/command/mod.ts";
+
+if (import.meta.main) {
+  const { options } = await new Command()
+    .name("sample")
+    .option("--in <in:string>", "input filepath", {
+      conflicts: ["--stdin"],
+      required: true,
+    })
+    .option("--stdin [stdin:boolean]", "use stdin", {
+      conflicts: ["in"],
+      default: false,
+      required: true,
+    })
+    .option("--out <out:string>", "output filename")
+    .parse(Deno.args);
+  console.debug(options);
+}
