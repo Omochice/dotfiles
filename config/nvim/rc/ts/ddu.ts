9569a60ec2e4315370b1c94e0ff35fb121e1619e
denopendabot <denopendabot@github.com>
2023-09-08 00:30:55 +0000

    build(deps): bump ddu_vim from v3.5.1 to v3.6.0

--- config/nvim/rc/ts/ddu.ts
+++ config/nvim/rc/ts/ddu.ts
@@ -1,5 +1,5 @@
-import { BaseConfig } from "https://deno.land/x/ddu_vim@v3.5.1/types.ts";
-import { ConfigArguments } from "https://deno.land/x/ddu_vim@v3.5.1/base/config.ts";
+import { BaseConfig } from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
+import { ConfigArguments } from "https://deno.land/x/ddu_vim@v3.6.0/base/config.ts";
 
 const border = ["┌", "─", "┐", "│", "┘", "─", "└", "│"] as const;
 
