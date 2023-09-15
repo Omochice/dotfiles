2d211f1bf2ced9db093faf469194c8a86d941b9a
denopendabot <denopendabot@github.com>
2023-09-15 01:08:42 +0000

    build(deps): bump ddc_vim from v4.0.4 to v4.0.5

--- config/nvim/rc/ts/ddc.ts
+++ config/nvim/rc/ts/ddc.ts
@@ -1,5 +1,5 @@
-import { BaseConfig } from "https://deno.land/x/ddc_vim@v4.0.4/types.ts";
-import { ConfigArguments } from "https://deno.land/x/ddc_vim@v4.0.4/base/config.ts";
+import { BaseConfig } from "https://deno.land/x/ddc_vim@v4.0.5/types.ts";
+import { ConfigArguments } from "https://deno.land/x/ddc_vim@v4.0.5/base/config.ts";
 
 export class Config extends BaseConfig {
   override async config(args: ConfigArguments): Promise<void> {
