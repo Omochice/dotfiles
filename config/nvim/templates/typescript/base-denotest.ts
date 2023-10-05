8af9e01541803adc5f783fd8354a1bf9fcea6bd4
Omochice <mochice.mls.ntl@gmail.com>
2023-10-05 11:05:27 +0900

    feat: add deno test template

---
+++ config/nvim/templates/typescript/base-denotest.ts
@@ -0,0 +1,8 @@
+import { assertEquals } from "https://deno.land/std@0.202.0/assert/mod.ts";
+
+Deno.test("url test", async (t) => {
+  await t.step("sample step", () => {
+    const url = new URL("./foo.js", "https://deno.land/");
+    assertEquals(url.href, "https://deno.land/foo.js");
+  });
+});
