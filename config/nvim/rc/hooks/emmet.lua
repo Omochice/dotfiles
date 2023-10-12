2ddd49bd103894c9165ff074e073885033dd9287
Omochice <mochice.mls.ntl@gmail.com>
2023-10-13 01:04:52 +0900

    refactor: use hooks_file instead

---
+++ config/nvim/rc/hooks/emmet.lua
@@ -0,0 +1,9 @@
+-- lua_add {{{
+local vimx = require("artemis")
+vimx.keymap.set("i", "<Plug>(emmet-expand-abbr-with-cmd)", function()
+  vimx.fn.emmet.util.closePopup()
+  vimx.fn.emmet.expandAbbr(0, "")
+  -- NOTE: after expand. cursor position is: `div` => `<div|></div>`
+  vimx.cmd([[silent! normal! 1l]])
+end)
+-- }}}
