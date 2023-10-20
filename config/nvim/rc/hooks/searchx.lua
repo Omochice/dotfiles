a4004818107d2bbfe407f4e025c41c4a7ea3d742
Omochice <mochice.mls.ntl@gmail.com>
2023-10-21 04:08:04 +0900

    fix: if on M1mac, funcref cause error

--- config/nvim/rc/hooks/searchx.lua
+++ config/nvim/rc/hooks/searchx.lua
@@ -5,13 +5,17 @@ vimx.g.searchx = {
   scrollof = vimx.go.scrolloff,
   scrolltile = 0,
   markers = vimx.fn.split("ABCDEFGHIJKLMNOPQRSTUVWXYZ", [[.\zs]]),
-  convert = function(input)
-    if vimx.fn.matchstr(input, [[\k]]) == "" then
-      return [[\V]] .. input
-    end
-    return table.concat(vimx.fn.split(input, " "), [[.\{-}]])
-  end
 }
+
+-- NOTE: M1だとfuncrefが通らないので
+vimx.cmd([[
+function g:searchx.convert(input) abort
+  if a:input !~# '\k'
+    return '\V' .. a:input
+  endif
+  return join(split(a:input, ' '), '.\{-}')
+endfunction
+]])
 
 vimx.keymap.set({ "n", "x" }, "?", function()
   vimx.fn.searchx.start({ dir = 0 })
@@ -19,10 +23,10 @@ end)
 vimx.keymap.set({ "n", "x" }, "/", function()
   vimx.fn.searchx.start({ dir = 1 })
 end)
-vimx.keymap.set({"n", "x"}, "N", function()
+vimx.keymap.set({ "n", "x" }, "N", function()
   vimx.fn.searchx.prev()
 end)
-vimx.keymap.set({"n", "x"}, "n", function()
+vimx.keymap.set({ "n", "x" }, "n", function()
   vimx.fn.searchx.next()
 end)
 -- }}}
