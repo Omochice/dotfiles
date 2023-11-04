f716f9e515ede6d26cb07b3a1efeb30ae0b334e1
Omochice <mochice.mls.ntl@gmail.com>
2023-11-05 04:43:26 +0900

    fix: fix typo

--- config/nvim/rc/hooks/emmet.lua
+++ config/nvim/rc/hooks/emmet.lua
@@ -1,6 +1,6 @@
 -- lua_add {{{
 local vimx = require("artemis")
-vimx.g.user_emment_install_global = false
+vimx.g.user_emmet_install_global = false
 vimx.keymap.set("i", "<Plug>(emmet-expand-abbr-with-cmd)", function()
   vimx.fn.emmet.util.closePopup()
   vimx.fn.emmet.expandAbbr(0, "")
