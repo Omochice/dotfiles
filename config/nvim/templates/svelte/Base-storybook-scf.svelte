167ddb728c0e745e2e25429dbbfdcee6c8c0c733
Omochice <mochice.mls.ntl@gmail.com>
2024-01-05 19:58:57 +0900

    feat: add svelte storybook template

---
+++ config/nvim/templates/svelte/Base-storybook-scf.svelte
@@ -0,0 +1,21 @@
+<script context="module">
+  import ComponentName from "./ComponentName.svelte";
+
+  export const meta = {
+    title:"ComponentName",
+    component:ComponentName,
+  };
+</script>
+
+<script>
+  import { Template, Story } from "@storybook/addon-svelte-csf";
+</script>
+
+<Template let:args>
+  <ComponentName />
+</Template>
+
+<Story
+  name="Primary"
+  args={{}}
+/>
