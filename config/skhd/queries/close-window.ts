83879ebc36336b64516e03a02ef79a92a6eafe8a
Omochice <mochice.mls.ntl@gmail.com>
2024-01-08 05:40:22 +0900

    feat: replace deno_yabai

---
+++ config/skhd/queries/close-window.ts
@@ -0,0 +1,19 @@
+import { close, focus } from "https://deno.land/x/deno_yabai@v0.1.0/window.ts";
+import { getSpaces } from "https://deno.land/x/deno_yabai@v0.1.0/query.ts";
+import { errAsync, okAsync } from "npm:neverthrow@6.1.0";
+
+if (import.meta.main) {
+  await close()
+    .andThen(() => getSpaces())
+    .andThen(
+      (spaces) => {
+        const currentSpace = spaces
+          .map((space) => space["first-window"])
+          .at(0);
+        return currentSpace == null
+          ? errAsync(new Error("currentSpace is nully"))
+          : okAsync(currentSpace);
+      },
+    )
+    .andThen((target) => focus({ target }));
+}
