98408667371b5bef63d82daf356b023259b962e8
Omochice <mochice.mls.ntl@gmail.com>
2024-02-07 09:44:12 +0900

    refactor: refactor f

--- config/fish/functions/f.fish
+++ config/fish/functions/f.fish
@@ -1,17 +1,17 @@
 function f --description "fuzzy moving with ghq"
   if ! type "ghq" 2>&1 >/dev/null
     echo "ghq is not included into PATH"
-    false
+    return (false)
   end
   if ! type "fzf" 2>&1 >/dev/null
     echo "fzf is not included into PATH"
-    false
+    return (false)
   end
   set --local root (ghq root)
 
   set --local repo (ghq list | fzf --preview "cat '$root/{}/README.md' || echo 'NO README'")
   if test -z "$repo"
-    false
+    return (false)
   end
-  cd $root/$repo
+  cd "$root"/"$repo"
 end
