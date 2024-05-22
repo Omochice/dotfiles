1e5473067a0e27674a0b2c341b8e98eca71fd545
Omochice <mochice.mls.ntl@gmail.com>
2024-05-22 17:46:19 +0900

    feat: add mapping to open current in oil

--- config/nvim/rc/hooks/fern.lua
+++ config/nvim/rc/hooks/fern.lua
@@ -1,6 +1,7 @@
 -- lua_add {{{
 local vimx = require("artemis")
 vimx.g["fern#disable_default_mappings"] = true
+vimx.g["fern#default_hidden"] = true
 vimx.keymap.set("ca", "fe", "Fern .")
 vimx.keymap.set("ca", "fep", "Fern . -reveal=%")
 vimx.create_command("TFern", function()
@@ -16,8 +17,6 @@ vimx.keymap.set("n", ";;", "<Cmd>Fern . -reveal=% -drawer -toggle<CR>")
 -- lua_fern {{{
 local vimx = require("artemis")
 vimx.keymap.set("n", "q", "<Cmd>bprevious<CR>", { buffer = true, nowait = true })
-vimx.keymap.set("n", "i", "<Plug>(fern-action-new-file)", { buffer = true, nowait = true })
-vimx.keymap.set("n", "o", "<Plug>(fern-action-new-dir)", { buffer = true })
 vimx.keymap.set("n", "r", "<Plug>(fern-action-rename)", { buffer = true })
 vimx.keymap.set("n", "dd", "<Plug>(fern-action-remove=)", { buffer = true })
 vimx.keymap.set("n", "yy", "<Plug>(fern-action-clipboard-copy)", { buffer = true })
@@ -40,4 +39,12 @@ vimx.keymap.set("n", "<CR>",
   end,
   { buffer = true, expr = true }
 )
+vimx.keymap.set("n", "<Plug>(fern-action-open-here-in-oil)",
+  function()
+    vim.cmd [[ let g:_tmp_fern_oil = fern#helper#new().sync.get_cursor_node()._path->fnamemodify(':h') ]]
+    require("oil").open_float(vim.g["_tmp_fern_oil"])
+    vim.cmd [[ unlet g:_tmp_fern_oil ]]
+  end
+)
+vimx.keymap.set("n", "i", "<Plug>(fern-action-open-here-in-oil)", { buffer = true })
 -- }}}
