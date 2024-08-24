8b867c1ddae12a29a0abfe1770fdd9dbf25b9ece
Omochice <mochice.mls.ntl@gmail.com>
2024-08-25 01:36:47 +0900

    feat: move spotlight config to nix

--- flake.nix
+++ flake.nix
@@ -73,6 +73,96 @@
                 HideDesktop = false;
                 EnableStandardClickToShowDesktop = false;
               };
+              CustomUserPreferences = {
+                "com.apple.Spotlight" = {
+                  orderedItems = [
+                    {
+                      enabled = false;
+                      name = "PDF";
+                    }
+                    {
+                      enabled = false;
+                      name = "MENU_SPOTLIGHT_SUGGESTIONS";
+                    }
+                    {
+                      enabled = false;
+                      name = "BOOKMARKS";
+                    }
+                    {
+                      enabled = true;
+                      name = "APPLICATIONS";
+                    }
+                    {
+                      enabled = false;
+                      name = "EVENT_TODO";
+                    }
+                    {
+                      enabled = false;
+                      name = "SYSTEM_PREFS";
+                    }
+                    {
+                      enabled = false;
+                      name = "SPREADSHEETS";
+                    }
+                    {
+                      enabled = false;
+                      name = "MENU_OTHER";
+                    }
+                    {
+                      enabled = false;
+                      name = "TIPS";
+                    }
+                    {
+                      enabled = false;
+                      name = "DIRECTORIES";
+                    }
+                    {
+                      enabled = false;
+                      name = "FONTS";
+                    }
+                    {
+                      enabled = false;
+                      name = "PRESENTATIONS";
+                    }
+                    {
+                      enabled = false;
+                      name = "MUSIC";
+                    }
+                    {
+                      enabled = false;
+                      name = "MOVIES";
+                    }
+                    {
+                      enabled = false;
+                      name = "MESSAGES";
+                    }
+                    {
+                      enabled = false;
+                      name = "IMAGES";
+                    }
+                    {
+                      enabled = false;
+                      name = "MENU_EXPRESSION";
+                    }
+                    {
+                      enabled = false;
+                      name = "DOCUMENTS";
+                    }
+                    {
+                      enabled = false;
+                      name = "MENU_DEFINITION";
+                    }
+                    {
+                      enabled = false;
+                      name = "MENU_CONVERSION";
+                    }
+                    {
+                      enabled = false;
+                      name = "CONTACT";
+                    }
+                  ];
+                };
+              };
             };
           };
         };
--- scripts/mac-defaults.sh
+++ scripts/mac-defaults.sh
@@ -24,29 +24,4 @@ defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 64 "
   </dict>
 </dict>
 "
-# Show only application
-defaults write com.apple.Spotlight orderedItems -array \
-  "{ enabled = 0; name = PDF; }" \
-  "{ enabled = 0; name = 'MENU_SPOTLIGHT_SUGGESTIONS'; }" \
-  "{ enabled = 0; name = BOOKMARKS; }" \
-  "{ enabled = 1; name = APPLICATIONS; }" \
-  "{ enabled = 0; name = 'EVENT_TODO'; }" \
-  "{ enabled = 0; name = 'SYSTEM_PREFS'; }" \
-  "{ enabled = 0; name = SPREADSHEETS; }" \
-  "{ enabled = 0; name = 'MENU_OTHER'; }" \
-  "{ enabled = 0; name = TIPS; }" \
-  "{ enabled = 0; name = DIRECTORIES; }" \
-  "{ enabled = 0; name = FONTS; }" \
-  "{ enabled = 0; name = PRESENTATIONS; }" \
-  "{ enabled = 0; name = MUSIC; }" \
-  "{ enabled = 0; name = MOVIES; }" \
-  "{ enabled = 0; name = MESSAGES; }" \
-  "{ enabled = 0; name = IMAGES; }" \
-  "{ enabled = 0; name = 'MENU_EXPRESSION'; }" \
-  "{ enabled = 0; name = DOCUMENTS; }" \
-  "{ enabled = 0; name = 'MENU_DEFINITION'; }" \
-  "{ enabled = 0; name = 'MENU_CONVERSION'; }" \
-  "{ enabled = 0; name = CONTACT; }"
-## }}}
-
-# vim:foldmethod=marker
+ vim:foldmethod=marker
