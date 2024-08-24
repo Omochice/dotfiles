21b15903da4913cd90ea886cd7400b3af507dd03
Omochice <mochice.mls.ntl@gmail.com>
2024-08-25 01:56:24 +0900

    feat: move symbolichotkeys into nix

--- flake.nix
+++ flake.nix
@@ -162,6 +162,46 @@
                     }
                   ];
                 };
+                symbolichotkeys = {
+                  AppleSymbolicHotKeys = {
+                    # Disable S-Space
+                    "60" = {
+                      enabled = false;
+                      value = {
+                        parameters = [
+                          32
+                          49
+                          262144
+                        ];
+                        type = "standard";
+                      };
+                    };
+                    "61" = {
+                      enabled = false;
+                      value = {
+                        parameters = [
+                          32
+                          49
+                          786432
+                        ];
+                        type = "standard";
+                      };
+                    };
+                    # Show spotlight on Mod-r
+                    "64" = {
+                      enabled = true;
+                      value = {
+                        parameters = [
+                          114
+                          15
+                          1048576
+                        ];
+                        type = "standard";
+                      };
+                    };
+                  };
+
+                };
               };
             };
           };
