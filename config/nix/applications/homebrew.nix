1ecfe99bfe58f381f7a2f81afe144ee490a5bddd
Omochice <mochice.mls.ntl@gmail.com>
2024-08-25 19:39:04 +0900

    feat: add casks into nix module

---
+++ config/nix/applications/homebrew.nix
@@ -0,0 +1,17 @@
+{ ... }:
+{
+  homebrew = {
+    casks = [
+      "alt-tab"
+      "android-studio"
+      "aquaskk"
+      "discord"
+      "gyazo"
+      "karabiner-elements"
+      "scroll-reverser"
+      "slack"
+      "vivaldi"
+      "wezterm"
+    ];
+  };
+}
