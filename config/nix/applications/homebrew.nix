439b586ed407ca730791b0f627f763c092293bee
Omochice <mochice.mls.ntl@gmail.com>
2024-09-10 23:51:40 +0900

    chore: update brew nix module

--- config/nix/applications/homebrew.nix
+++ config/nix/applications/homebrew.nix
@@ -1,6 +1,35 @@
 { ... }:
 {
   homebrew = {
+    enable = true;
+    onActivation = {
+      autoUpdate = true;
+    };
+    taps = [
+      "felixkratz/formulae"
+      "koekeishiya/formulae"
+      "aquaproj/aqua"
+    ];
+    brews = [
+      "aquaproj/aqua/aqua"
+      "btop"
+      "cmake"
+      "curl"
+      "fish"
+      "gettext"
+      "libtool"
+      "mise"
+      "ninja"
+      "pkg-config"
+      "starship"
+      "tldr"
+      "pnpm"
+      "make"
+      "nushell"
+      "pandoc"
+      "koekeishiya/formulae/yabai"
+      "felixkratz/formulae/sketchybar"
+    ];
     casks = [
       "alt-tab"
       "android-studio"
