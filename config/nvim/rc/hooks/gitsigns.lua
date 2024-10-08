cde5499171e2a0bc093675fe78add9541211e762
Omochice <mochice.mls.ntl@gmail.com>
2024-10-08 11:45:45 +0900

    feat: use gitsign blame_line instead on nvim

--- config/nvim/rc/hooks/gitsigns.lua
+++ config/nvim/rc/hooks/gitsigns.lua
@@ -2,9 +2,9 @@
 local gitsign = require("gitsigns")
 gitsign.setup({
   on_attach = function(_bufnr)
-    local vimx = require("artemis")
-    vimx.keymap.set("n", "<C-g><C-p>", gitsign.prev_hunk, { buffer = true })
-    vimx.keymap.set("n", "<C-g><C-n>", gitsign.next_hunk, { buffer = true })
+    vim.keymap.set("n", "<C-g><C-p>", gitsign.prev_hunk, { buffer = true })
+    vim.keymap.set("n", "<C-g><C-n>", gitsign.next_hunk, { buffer = true })
+    vim.keymap.set("n", "<Space>gm", gitsign.blame_line, { buffer = true })
   end
 })
 -- }}}
