8568d6c566304181414aa6dc1328b9c5426d92d6
Omochice <mochice.mls.ntl@gmail.com>
2024-10-28 10:43:09 +0900

    fix: glab-cli needs 600
    Dirty hack

--- config/glab-cli/default.nix
+++ config/glab-cli/default.nix
@@ -1,6 +1,12 @@
-{ ... }:
+{ config, ... }:
 {
   xdg.configFile = {
-    "glab-cli/aliases.yml".text = builtins.readFile ./aliases.yml;
+    "glab-cli/dummy" = {
+      text = builtins.readFile ./aliases.yml;
+      # FIXME: glab-cli needs set 600 to `aliases.yml`, but home-manager generate 444.
+      onChange = ''
+        ln -snf ${config.home.homeDirectory}/dotfiles/aliases.yml ${config.xdg.configHome}/glab-cli/aliases.yml
+      '';
+    };
   };
 }
