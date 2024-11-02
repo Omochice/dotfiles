ba3691af91057a539d5ff43f0196399c05ebfec2
Omochice <mochice.mls.ntl@gmail.com>
2024-11-03 00:41:05 +0900

    chore: need 600

--- config/glab-cli/default.nix
+++ config/glab-cli/default.nix
@@ -6,6 +6,7 @@
       # FIXME: glab-cli needs set 600 to `aliases.yml`, but home-manager generate 444.
       onChange = ''
         ln -snf ${config.home.homeDirectory}/dotfiles/config/glab-cli/aliases.yml ${config.xdg.configHome}/glab-cli/aliases.yml
+        chmod 600 ${config.xdg.configHome}/glab-cli/aliases.yml
       '';
     };
   };
