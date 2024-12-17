5f3e9e719db63a8e651842019523c26ce11bab97
Omochice <mochice.mls.ntl@gmail.com>
2024-12-17 15:21:50 +0900

    feat: add kana table for macskk

---
+++ config/macskk/default.nix
@@ -0,0 +1,8 @@
+{ ... }:
+{
+  home.file = {
+    "Library/Containers/net.mtgto.inputmethod.macSKK/Data/Documents/Settings/kana-rule.conf" = {
+      source = ./kana-rule.conf;
+    };
+  };
+}
--- config/nix/home-manager/home.nix
+++ config/nix/home-manager/home.nix
@@ -23,6 +23,7 @@ in
     ../../yabai/default.nix
     ../../wezterm/default.nix
     ../../nvim/default.nix
+    ../../macskk/default.nix
   ];
   nixpkgs = {
     config = {
