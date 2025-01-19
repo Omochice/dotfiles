32a3c3d87d2e799e08ef7ab7867fd6231acbc875
Omochice <mochice.mls.ntl@gmail.com>
2025-01-20 02:48:44 +0900

    revert: revert 844c0fbfe3beb2c83e4d9c3390140e737fd0ef59
    This commit reverts 844c0fbfe3beb2c83e4d9c3390140e737fd0ef59

--- config/nix/home-manager/home.nix
+++ config/nix/home-manager/home.nix
@@ -6,12 +6,9 @@
   ...
 }:
 let
-  loadFlake = flakePath: (builtins.getFlake (toString flakePath)).packages.${pkgs.system}.default;
   # FIXME: I dont want to write user name here, but it doesn't work without "--impure"
   username = builtins.getEnv "USER";
   homeDirectory = builtins.getEnv "HOME";
-  firgeFont = loadFlake ../../font/firge;
-  firgeNerdFont = loadFlake ../../font/firge-nerd;
 in
 {
   imports = [
@@ -57,8 +54,8 @@ in
       starship
       tldr
       nix-output-monitor
-      firgeFont
-      firgeNerdFont
+      (callPackage ../../font/firge-nerd/package.nix { })
+      (callPackage ../../font/firge/package.nix { })
     ];
   };
   programs.home-manager.enable = true;
