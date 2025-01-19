142cbc71ffd4e9039909f6ab21e2fcc48b582141
Omochice <mochice.mls.ntl@gmail.com>
2025-01-19 16:31:03 +0900

    feat: add perplexity api provider

--- config/nvim/rc/hooks/parrot.lua
+++ config/nvim/rc/hooks/parrot.lua
@@ -1,11 +1,18 @@
 -- lua_source {{{
-require("parrot").setup({
-  providers = {
-    ollama = {
-      topic = {
-        model = "qwen2.5-coder:32b",
-      },
+local providers = {
+  ollama = {
+    topic = {
+      model = "qwen2.5-coder:32b",
     },
   },
+}
+local perplexty_api_key = os.getenv("PERPLEXITY_API_KEY")
+if perplexty_api_key ~= nil and perplexty_api_key ~= "" then
+  providers.pplx = {
+    api_key = perplexty_api_key,
+  }
+end
+require("parrot").setup({
+  providers = providers
 })
 -- }}}
