ec28c6a41ef3dec6a5e16fd027f18c7da064d549
Omochice <mochice.mls.ntl@gmail.com>
2025-04-07 15:08:00 +0900

    chore: use nix-darwin/nix-darwin instead

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nix-darwin = {
-      url = "github:LnL7/nix-darwin";
+      url = "github:nix-darwin/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
