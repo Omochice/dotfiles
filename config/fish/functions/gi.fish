e70edd93032eca740afae679ea144114b163ccdf
Omochice <mochice.mls.ntl@gmail.com>
2025-04-20 06:27:10 +0900

    refactor: use gh api instead

--- config/fish/functions/gi.fish
+++ config/fish/functions/gi.fish
@@ -1,3 +1,9 @@
-function gi
-	curl -sL https://www.toptal.com/developers/gitignore/api/$argv
+function gi --description "gitignore"
+  set --local selected (gh api gitignore/templates | jq -r ".[]" | fzf --no-sort --multi --preview="gh api gitignore/templates/{} | jq -r '.source'" --height=50% --bind 'ctrl-d:preview-half-page-down,ctrl-u:preview-half-page-up')
+  if test -z "$selected"
+    return 1
+  end
+  for lang in "$selected"
+    gh api gitignore/templates/$lang | jq -r ".source"
+  end
 end
