22f0d29eab6d707240118abecffc4dac222f9bfc
Omochice <mochice.mls.ntl@gmail.com>
2025-04-28 15:03:25 +0900

    refactor: use `[c` instead

--- config/nvim/rc/hooks/gitsigns.lua
+++ config/nvim/rc/hooks/gitsigns.lua
@@ -1,10 +1,13 @@
 -- lua_source {{{
 local gitsign = require("gitsigns")
 gitsign.setup({
-  on_attach = function(_bufnr)
-    vim.keymap.set("n", "<C-g><C-p>", gitsign.prev_hunk, { buffer = true })
-    vim.keymap.set("n", "<C-g><C-n>", gitsign.next_hunk, { buffer = true })
-    vim.keymap.set("n", "<Space>gm", gitsign.blame_line, { buffer = true })
+  on_attach = function(bufnr)
+    local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
+    if filetype ~= "diff" then
+      vim.keymap.set("n", "[c", gitsign.prev_hunk, { buffer = bufnr })
+      vim.keymap.set("n", "]c", gitsign.next_hunk, { buffer = bufnr })
+    end
+    vim.keymap.set("n", "<Space>gm", gitsign.blame_line, { buffer = bufnr })
   end,
 })
 -- }}}
