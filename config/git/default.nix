707f2b2a1fb1692936d939099cf62c8f91b4c1e2
Omochice <mochice.mls.ntl@gmail.com>
2025-05-20 12:50:34 +0900

    feat: add dd command
    delete detached

--- config/git/default.nix
+++ config/git/default.nix
@@ -1,10 +1,20 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 {
   programs.git = {
     enable = true;
     includes = [
       { path = ./config; }
     ];
+    aliases = {
+      dd = ''
+        !f() {
+          ${pkgs.git}/bin/git branch -vv \
+          | ${pkgs.gnugrep}/bin/grep ': gone]' \
+          | ${pkgs.gawk}/bin/awk '{print $1}' \
+          | ${pkgs.findutils}/bin/xargs -r ${pkgs.git}/bin/git branch -D
+        };f
+      '';
+    };
     ignores = ./ignore |> builtins.readFile |> lib.splitString "\n";
     extraConfig = {
       commit.template = ./gitmessage |> toString;
