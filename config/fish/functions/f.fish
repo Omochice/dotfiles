23d1b961f44b0a2d4859e078080a9b3fc5087489
Omochice <mochice.mls.ntl@gmail.com>
2025-06-12 17:17:38 +0900

    fix: show readme on multiple root

--- config/fish/functions/f.fish
+++ config/fish/functions/f.fish
@@ -1,26 +1,34 @@
-function f --description "fuzzy moving with ghq"
-  if ! type "ghq" 2>&1 >/dev/null
-    echo "ghq is not included into PATH"
-    return (false)
-  end
-  if ! type "fzf" 2>&1 >/dev/null
-    echo "fzf is not included into PATH"
-    return (false)
-  end
-  set --local roots (ghq root --all)
-
-  set --local repo (ghq list | fzf --preview "cat '$root/{}/README.md' || echo 'NO README'" --query "$argv[1]")
-  if test -z "$repo"
-    return (false)
-  end
-
-  for root in $roots
-    set --local p "$root"/"$repo"
-    if test -d "$p"
-      cd "$p"
-      return (true)
+function f --description="fuzzy moving with ghq"
+    if ! type ghq 2>&1 >/dev/null
+        echo "ghq is not included into PATH"
+        return (false)
     end
-  end
-  # unreachable because `ghq list` show only repos into `ghq root --all`
-  return (false)
+    if ! type fzf 2>&1 >/dev/null
+        echo "fzf is not included into PATH"
+        return (false)
+    end
+    if ! type bat 2>&1 >/dev/null
+        echo "bat is not included into PATH"
+        return (false)
+    end
+    set --local roots (ghq root --all)
+    set --local repo (ghq list | fzf --preview "
+      for root in $roots
+        if bat --color=always --plain \$root/{}/README.md 2>/dev/null
+          exit
+        end
+      end
+      echo 'NO README'" --query "$argv[1]")
+    if test -z "$repo"
+        return (false)
+    end
+    for root in $roots
+        set --local p "$root"/"$repo"
+        if test -d "$p"
+            cd "$p"
+            return (true)
+        end
+    end
+    # unreachable because `ghq list` show only repos into `ghq root --all`
+    return (false)
 end
