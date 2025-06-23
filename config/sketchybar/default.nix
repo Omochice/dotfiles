c25d8f62fee46f0bf9c2739c1c8acc8d458d41bf
Omochice <mochice.mls.ntl@gmail.com>
2025-06-24 02:29:36 +0900

    feat: show ccusage on sketchybar

--- config/sketchybar/default.nix
+++ config/sketchybar/default.nix
@@ -1,8 +1,18 @@
-{ ... }:
+{ pkgs, ... }:
 {
   xdg.configFile = {
     sketchybar = {
       source = ./.;
     };
+    "skt/ccusage.sh" = {
+      text = ''
+        #!/usr/bin/env bash
+        set -ue
+        COST="$(${pkgs.ccusage}/bin/ccusage --offline --json | ${pkgs.jq}/bin/jq -r '{ cost: .totals.totalCost, daily: .daily[-1].totalCost } | map_values((. * 100 | ceil ) / 100) | "$\(.cost) ($\(.daily)/d)"')"
+        sketchybar -m --set $NAME label="$COST"
+      '';
+      executable = true;
+    };
+
   };
 }
