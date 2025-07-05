4151a8eeeba9d1e67b9ea4b315c8ce26a378896f
Omochice <mochice.mls.ntl@gmail.com>
2025-07-06 03:00:35 +0900

    fix: dont block pipefail on gh-q

--- config/gh/default.nix
+++ config/gh/default.nix
@@ -4,34 +4,90 @@ let
     pkgs.callPackage ../../_sources/generated.nix { }
     |> lib.attrsets.filterAttrs (name: _: name |> lib.strings.hasPrefix "gh-");
   runAs =
-    name: runtimeInputs: text:
+    name:
+    {
+      runtimeInputs,
+      bashOptions,
+      excludeShellChecks,
+    }:
+    text:
     pkgs.writeShellApplication {
-      inherit name runtimeInputs text;
-      excludeShellChecks = [
-        "SC2016"
-        "SC2059"
-        "SC2086"
-        "SC2181"
-      ];
+      inherit
+        name
+        runtimeInputs
+        text
+        bashOptions
+        excludeShellChecks
+        ;
     };
   readShellExetension =
-    name: depends: builtins.readFile "${extensions.${name}.src}/${name}" |> runAs name depends;
-  gh-extensions = [
-    (readShellExetension "gh-q" [
-      pkgs.gh
-      pkgs.fzf
-      pkgs.coreutils
-    ])
-    (readShellExetension "gh-fzgist" [
-      pkgs.gh
-      pkgs.fzf
-      pkgs.gawk
-      pkgs.findutils
-    ])
-    (readShellExetension "gh-userfetch" [
-      pkgs.jq
-      pkgs.coreutils
-    ])
+    {
+      src,
+      depends,
+      bashOptions ? [
+        "errexit"
+        "nounset"
+        "pipefail"
+      ],
+      excludeShellChecks ? [],
+    }:
+    (builtins.readFile "${src.src}/${src.pname}")
+    |> runAs src.pname {
+      inherit bashOptions excludeShellChecks;
+      runtimeInputs = depends;
+    }
+    |> (program: {
+      name = program.name;
+      path = "${program}/bin";
+    });
+
+  shell-extensions =
+    [
+      {
+        src = extensions.gh-q;
+        depends = [
+          pkgs.gh
+          pkgs.ghq
+          pkgs.fzf
+          pkgs.coreutils
+        ];
+        bashOptions = [
+          "errexit"
+          "nounset"
+          # NOTE: If you confirm before you accumulate enough candidates, gh api will pipefail.
+        ];
+        excludeShellChecks = [
+          "SC2016"
+          "SC2181"
+        ];
+      }
+      {
+        src = extensions.gh-fzgist;
+        depends = [
+          pkgs.gh
+          pkgs.fzf
+          pkgs.gawk
+          pkgs.findutils
+        ];
+      }
+      {
+        src = extensions.gh-userfetch;
+        depends = [
+          pkgs.jq
+          pkgs.coreutils
+        ];
+        excludeShellChecks = [
+          "SC2059"
+          "SC2086"
+        ];
+      }
+    ]
+    |> builtins.map (ext: readShellExetension ext);
+  native-extensions = [
+    {
+      name = "gh-dash";
+      path = "${pkgs.gh-dash}/bin";
+    }
   ];
 in
 {
@@ -39,20 +95,6 @@ in
     "gh/config.yml".source = ./config.yml;
   };
   xdg.dataFile."gh/extensions" = {
-    source = pkgs.linkFarm "gh-extensions" (
-      (
-        gh-extensions
-        |> builtins.map (p: {
-          name = p.name;
-          path = "${p}/bin";
-        })
-      )
-      ++ [
-        {
-          name = "gh-dash";
-          path = "${pkgs.gh-dash}/bin";
-        }
-      ]
-    );
+    source = pkgs.linkFarm "gh-extensions" (shell-extensions ++ native-extensions);
   };
 }
