e34cdb65ae6237af077d85d6afd1dc779a5407bf
Omochice <mochice.mls.ntl@gmail.com>
2025-07-07 18:21:38 +0900

    refactor: preview with bat

--- config/zk/default.nix
+++ config/zk/default.nix
@@ -1,7 +1,22 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.zk = {
     enable = true;
-    settings = ./config.toml |> builtins.readFile |> builtins.fromTOML;
+    settings = {
+      note = {
+        language = "ja";
+        default-title = "Untitled";
+        filename = "{{id}}-{{slug title}}";
+        extension = "md";
+        template = "default.md";
+        id-charset = "alphanum";
+        id-length = 4;
+        id-case = "lower";
+      };
+      tool = {
+        editor = "nvim";
+        fzf-preview = "${pkgs.bat}/bin/bat -p --color always {-1}";
+      };
+    };
   };
 }
