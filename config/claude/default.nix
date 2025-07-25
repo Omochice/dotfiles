ba3d0df2b3ce79da174e5fcebbe3dc7cf94d5e81
Omochice <mochice.mls.ntl@gmail.com>
2025-07-26 01:15:50 +0900

    chore: omit $schema when put

--- config/claude/default.nix
+++ config/claude/default.nix
@@ -1,7 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 {
   xdg.configFile = {
-    "claude/settings.json".source = ./settings.json;
+    "claude/settings.json".text =
+      ./settings.json
+      |> builtins.readFile
+      |> builtins.fromJSON
+      |> pkgs.lib.attrsets.filterAttrs (k: _: k != "$schema")
+      |> builtins.toJSON;
     "claude/CLAUDE.md".source = ./CLAUDE.md;
     "claude/commands".source = ./commands;
   };
