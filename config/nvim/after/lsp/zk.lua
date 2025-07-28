9d79bfd3a5187864ccfaec6b5bad2c510bb6ce68
Omochice <mochice.mls.ntl@gmail.com>
2025-07-28 12:51:43 +0900

    fixup! refactor: migrate to `vim.lsp.enable` pattern for nvim >= 0.11

--- config/nvim/after/lsp/zk.lua
+++ config/nvim/after/lsp/zk.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-require("lua/lsp-helper").on_attach("zk", function()
+require("vimrc/lsp-helper").on_attach("zk", function()
   local win = vim.api.nvim_get_current_win()
   vim.wo[win][0].foldmethod = "expr"
   vim.wo[win][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
