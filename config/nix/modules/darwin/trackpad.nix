1801c660829da3c19a237a1d66663be2d4fc6bc3
Omochice <mochice.mls.ntl@gmail.com>
2025-11-04 10:36:56 +0900

    fix: delete `system.defaults.trackpad.TrackpadMomentumScroll`
    Now, it is provided by nix-darwin.

--- config/nix/modules/darwin/trackpad.nix
+++ config/nix/modules/darwin/trackpad.nix
@@ -4,14 +4,6 @@ with lib;
 
 {
   options = {
-    system.defaults.trackpad.TrackpadMomentumScroll = mkOption {
-      type = types.nullOr types.bool;
-      default = null;
-      description = ''
-        Whether or not to enable momentum scrolling on trackpad.
-        Same as `defaults write com.apple.AppleMultitouchTrackpad TrackpadMomentumScroll`.
-      '';
-    };
     system.defaults.trackpad.ForceClick = mkOption {
       type = types.nullOr types.bool;
       default = null;
