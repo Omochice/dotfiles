7a9d6802173d3c21085ccb22c05efd47694b05e8
Omochice <mochice.mls.ntl@gmail.com>
2025-11-12 21:20:53 +0900

    fix: stop to map <C-n> <C-p> on prompt
    I want to use them for selecting next/prev pum item.

--- config/nvim/rc/hooks/aibo.lua
+++ config/nvim/rc/hooks/aibo.lua
@@ -13,11 +13,25 @@ require("aibo").setup({
       vim.keymap.set({ "n", "i" }, "<C-c>", "<Plug>(aibo-send)<Esc>", opts)
       vim.keymap.set({ "n", "i" }, "g<C-c>", "<Plug>(aibo-send)<C-c>", opts)
       vim.keymap.set({ "n", "i" }, "<C-l>", "<Plug>(aibo-send)<C-l>", opts)
-      vim.keymap.set({ "n", "i" }, "<C-n>", "<Plug>(aibo-send)<C-n>", opts)
-      vim.keymap.set({ "n", "i" }, "<C-p>", "<Plug>(aibo-send)<C-p>", opts)
       vim.keymap.set({ "n", "i" }, "<Down>", "<Plug>(aibo-send)<Down>", opts)
       vim.keymap.set({ "n", "i" }, "<Up>", "<Plug>(aibo-send)<Up>", opts)
     end,
+  },
+  tools = {
+    claude = {
+      no_default_mappings = true,
+      on_attach = function(bufnr)
+        local opts = { buffer = bufnr, nowait = true, silent = true }
+        vim.keymap.set({ "n", "i" }, "<Tab>", "<Plug>(aibo-send)<Tab>", opts)
+        vim.keymap.set({ "n", "i" }, "<S-Tab>", "<Plug>(aibo-send)<S-Tab>", opts)
+        vim.keymap.set({ "n", "i" }, "<F2>", "<Plug>(aibo-send)<F2>", opts)
+        vim.keymap.set({ "n", "i" }, "<C-o>", "<Plug>(aibo-send)<C-o>", opts)
+        vim.keymap.set({ "n", "i" }, "<C-t>", "<Plug>(aibo-send)<C-t>", opts)
+        vim.keymap.set({ "n", "i" }, "<C-_>", "<Plug>(aibo-send)<C-_>", opts)
+        vim.keymap.set({ "n", "i" }, "<C-->", "<Plug>(aibo-send)<C-_>", opts)
+        vim.keymap.set({ "n", "i" }, "<C-v>", "<Plug>(aibo-send)<C-v>", opts)
+      end,
+    },
   },
 })
 -- }}}
