d380e9c946d57db07f4ea38a82fe2cff6e3f2da3
Omochice <mochice.mls.ntl@gmail.com>
2025-11-17 14:50:44 +0900

    refactor: use lsp and diagnostic only

--- config/nvim/rc/hooks/hover.lua
+++ config/nvim/rc/hooks/hover.lua
@@ -14,16 +14,15 @@ end)
 require("hover").config({
   --- @type (string|Hover.Config.Provider)[]
   providers = {
-    "hover.providers.diagnostic",
     "hover.providers.lsp",
+    "hover.providers.diagnostic",
     "hover.providers.dap",
-    "hover.providers.man",
-    "hover.providers.dictionary",
   },
   preview_opts = {
     border = "single",
   },
   preview_window = false,
   title = true,
+  mouse_providers = {},
 })
 --- }}
