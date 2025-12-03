7116d6a6f47f12aed5de21e5eb1bb57fea1e124c
Omochice <mochice.mls.ntl@gmail.com>
2025-12-03 22:43:55 +0900

    feat: add incline

---
+++ config/nvim/rc/hooks/incline.lua
@@ -0,0 +1,75 @@
+-- lua_source {{{
+-- NOTE: This configuration is based on https://github.com/izumin5210/dotfiles/blob/15a954676994c3710926d4c9bbf1f635f3a145f4/config/.config/nvim/lua/utils/colors.lua
+local palette = vim.fn["sonokai#get_palette"](vim.g.sonokai_style, vim.empty_dict())
+-- local fg_active = palette.text
+local fg_inactive = palette.grey_dim[1]
+local icons = { error = "E:", warn = "W:", hint = "H:", info = "I:" }
+
+--- @class Prop
+--- @field buf number
+--- @field win number
+--- @field focused boolean
+
+--- @param props Prop
+local function get_diagnostic_label(props)
+  local label = {}
+
+  for severity, icon in pairs(icons) do
+    local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
+    if n > 0 then
+      table.insert(label, {
+        icon .. n .. " ",
+        group = props.focused and ("DiagnosticSign" .. severity) or "NonText",
+      })
+    end
+  end
+  if #label > 0 then
+    table.insert(label, 1, { ": ", guifg = fg_inactive })
+  end
+  return label
+end
+
+--- @param props Prop
+local function render(props)
+  local filename = vim.fn.bufname(props.buf)
+  local ft_icon = vim.fn["nerdfont#find"](filename)
+  local is_readonly = vim.bo[props.buf].readonly
+  local fg_filename_active = palette.fg[1]
+  local fg_filename = props.focused and fg_filename_active or fg_inactive
+
+  return {
+    {
+      (ft_icon and ft_icon .. " " or ""),
+      guifg = props.focused and fg_filename or fg_inactive,
+    },
+    {
+      (is_readonly and "RO " or ""),
+      guifg = fg_filename,
+    },
+    {
+      filename,
+      guifg = fg_filename,
+    },
+    {
+      vim.bo[props.buf].modified and " +" or "",
+      guifg = props.focused and palette.purple[1] or fg_inactive,
+    },
+    { get_diagnostic_label(props) },
+  }
+end
+
+require("incline").setup({
+  window = {
+    options = {
+      winblend = 0,
+    },
+    placement = {
+      horizontal = "left",
+      vertical = "bottom",
+    },
+    margin = { horizontal = 0, vertical = 0 },
+    padding = 2,
+  },
+  render = render,
+})
+-- }}}
