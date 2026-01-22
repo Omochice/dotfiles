30c7f306382d75c4a6663a706869d1e74b7e83fa
Omochice <mochice.mls.ntl@gmail.com>
2026-01-23 00:11:24 +0900

    feat: show graph on preview

--- config/fish/functions/t.fish
+++ config/fish/functions/t.fish
@@ -5,7 +5,7 @@ function t --description="fuzzy wotktree moving with ghq"
     end
     set --local parent (ghq root)/(echo "$repo" | string replace -r "^(https?|ssh)://" "" | path dirname)/
     set --local lines (git wt | string replace "$parent" "" |  string split \n)
-    set --local picked (printf '%s\n' $lines[2..] | fzf)
+    set --local picked (printf '%s\n' $lines[2..] | fzf --preview "git log --graph --oneline --color {-1}")
     set --local fields (string split -n ' ' -- $picked)
     if test -z "$fields"
         return (false)
