dd7b07e0b393edb60cce444d81e0bf80d3f2122f
Omochice <mochice.mls.ntl@gmail.com>
2026-01-23 12:38:55 +0900

    chore: move `$TERMINAL` into nix

--- config/wezterm/default.nix
+++ config/wezterm/default.nix
@@ -1,9 +1,14 @@
-{ ... }:
+{ lib, ... }:
 {
   xdg.configFile = {
     "wezterm" = {
       source = ./.;
       recursive = true;
     };
+  };
+  programs = {
+    fish.interactiveShellInit = lib.mkAfter ''
+      set --export --unpath TERMINAL wezterm
+    '';
   };
 }
