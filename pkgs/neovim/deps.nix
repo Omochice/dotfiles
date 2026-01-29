5a76b5e0b0fe3c5d63e208481433acdbd0612376
Omochice <44566328+Omochice@users.noreply.github.com>
2026-01-29 20:14:59 +0900

    refactor: kuuote/nixconf's neovim package instead of nvim-unwrapped (#1738)
    * refactor: use kuu's neovim package
    
    ref: https://github.com/kuuote/nixconf/tree/676cca2b8ce1edc367dc866bade3f3e892bc3035/pkgs/neovim
    
    https://www.kmonos.net/nysl/
    
    * fix: use clangStdenv if env is darwin
    
    * refactor: use lib.strings.splitString instead
    
    * refactor: remove redundant `builtins.`
    
    * ci: run flake check when update all .nix file
    
    * fixup! ci: run flake check when update all .nix file

---
+++ pkgs/neovim/deps.nix
@@ -0,0 +1,59 @@
+{
+  lib,
+  neovim-src,
+  pkgs,
+}:
+let
+  stdenv = if pkgs.stdenv.isDarwin then pkgs.clangStdenv else pkgs.useMoldLinker pkgs.clangStdenv;
+
+  moldRC =
+    name: env:
+    pkgs.runCommandWith {
+      inherit name stdenv;
+      runLocal = false;
+      derivationArgs = env;
+    };
+  deps =
+    "${neovim-src}/cmake.deps/deps.txt"
+    |> builtins.readFile
+    |> lib.strings.splitString "\n"
+    |> builtins.filter builtins.isString
+    |> map (builtins.match "([0-9A-Z_]+)_(URL|SHA256) (.+)")
+    |> builtins.filter builtins.isList
+    |> map (m: {
+      "${lib.toLower (builtins.elemAt m 0)}" = {
+        "${lib.toLower (builtins.elemAt m 1)}" = builtins.elemAt m 2;
+      };
+    })
+    |> builtins.foldl' lib.recursiveUpdate { }
+    |> lib.mapAttrs' (
+      name: value: {
+        name = "${name}/${baseNameOf value.url}";
+        value = builtins.fetchurl value;
+      }
+    )
+    |> pkgs.linkFarm "deps";
+  buildpath = builtins.path {
+    path = neovim-src;
+    name = "neovim-deps-build-path";
+    filter =
+      path: type:
+      let
+        list = builtins.filter builtins.isString (builtins.split "/" path);
+        base = builtins.elemAt list 4;
+      in
+      base == "Makefile" || base == "cmake" || base == "cmake.deps";
+    recursive = true;
+  };
+in
+moldRC "neovim-deps"
+  {
+    buildInputs = [ pkgs.cmake ];
+  }
+  ''
+    cp -a ${buildpath}/* .
+    mkdir -p .deps/build
+    cp -a ${deps} .deps/build/downloads
+    make -j$NIX_BUILD_CORES deps
+    cp -a .deps $out
+  ''
