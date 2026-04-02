cf4770d95a7f176fce9e9531be528fd9a4bdd39b
Omochice <mochice.mls.ntl@gmail.com>
2026-04-02 19:48:39 +0900

    refactor: treat `merged` as local

--- config/wezterm/utils.lua
+++ config/wezterm/utils.lua
@@ -1,14 +1,6 @@
 local M = {}
 
-function M.merged(tables)
-  local results = {}
-  for i = 1, #tables do
-    merged(results, tables[i])
-  end
-  return results
-end
-
-function merged(t1, t2) -- from [https://github.com/yutkat/dotfiles/blob/3576916618fa7991de69682f628ec4832cf919c7/.config/wezterm/utils.lua]
+local function merged(t1, t2) -- from [https://github.com/yutkat/dotfiles/blob/3576916618fa7991de69682f628ec4832cf919c7/.config/wezterm/utils.lua]
   for k, v in pairs(t2) do
     if (type(v) == "table") and (type(t1[k] or false) == "table") then
       merged(t1[k], t2[k])
@@ -17,6 +9,14 @@ function merged(t1, t2) -- from [https://github.com/yutkat/dotfiles/blob/3576916
     end
   end
   return t1
+end
+
+function M.merged(tables)
+  local results = {}
+  for i = 1, #tables do
+    merged(results, tables[i])
+  end
+  return results
 end
 
 function M.basename(path)
@@ -30,6 +30,5 @@ end
 function M.get_process_name(p)
   return M.basename(string.match(p, "^(.-)%s"))
 end
-
 
 return M
