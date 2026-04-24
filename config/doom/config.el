c0f3c46b7004a8bcbebd38700fa9d260e9e452b5
Omochice <mochice.mls.ntl@gmail.com>
2026-04-24 18:15:50 +0900

    feat(doom): add ddskk with AZIK and custom kana rules
    Port skkeleton configuration from Neovim setup: AZIK kana table
    with JIS layout, custom rules (xx-prefixed small kana, rr for
    れる, nn for なん), and : as henkan point key.

--- config/doom/config.el
+++ config/doom/config.el
@@ -1,3 +1,29 @@
 ;;; config.el -*- lexical-binding: t; -*-
 
 (setq confirm-kill-emacs nil)
+
+;; ddskk
+(setq default-input-method "japanese-skk")
+(setq skk-large-jisyo (expand-file-name "~/.cache/dpp/repos/github.com/skk-dev/dict/SKK-JISYO.L"))
+(setq skk-use-azik t)
+(setq skk-azik-keyboard-type 'jp)
+(setq skk-egg-like-newline t)           ; registerConvertResult equivalent
+(setq skk-henkan-point-key ":")         ; : を変換ポイントに
+(map! :i "C-j" #'skk-mode)
+
+(after! ddskk
+  ;; カスタムかなルール
+  (setq skk-rom-kana-rule-list
+        (append skk-rom-kana-rule-list
+                '(("xx" "x" ("ッ" . "っ"))
+                  ("xxa" nil ("ァ" . "ぁ"))
+                  ("xxi" nil ("ィ" . "ぃ"))
+                  ("xxu" nil ("ゥ" . "ぅ"))
+                  ("xxe" nil ("ェ" . "ぇ"))
+                  ("xxo" nil ("ォ" . "ぉ"))
+                  ("xxya" nil ("ャ" . "ゃ"))
+                  ("xxyu" nil ("ュ" . "ゅ"))
+                  ("xxyo" nil ("ョ" . "ょ"))
+                  ("xxwa" nil ("ヮ" . "ゎ"))
+                  ("rr" nil ("レル" . "れる"))
+                  ("nn" nil ("ナン" . "なん"))))))
