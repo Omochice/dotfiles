7ff02a3ec5157f6e12fb16ae973773f776ecbcb1
Omochice <mochice.mls.ntl@gmail.com>
2026-05-05 01:55:43 +0900

    feat: launch only denols if naked typescript file

--- config/nvim/after/lsp/tsgo.lua
+++ config/nvim/after/lsp/tsgo.lua
@@ -1,5 +1,14 @@
+local helper = require("vimrc/lsp-helper")
+
 ---@type vim.lsp.Config
 local M = {
+  root_dir = function(bufnr, callback)
+    local found = helper.root_pattern("tsconfig.json", "node_modules")(bufnr)
+    if found == nil then
+      return
+    end
+    callback(vim.fs.dirname(found))
+  end,
   single_file_support = false,
 }
 
