6f07768b5a6bc4b8d99415d42696ea3110521eb9
Omochice <mochice.mls.ntl@gmail.com>
2026-05-28 13:55:29 +0900

    fix: move npmrc into xdg-config-home
    deno 2.8 load $HOME/.npmrc automatically.
    
    denops plugins is blocked because min-release-age.
    
    Settings SHOULD NOT be shared if the purpose of use is different.

--- config/npm/default.nix
+++ config/npm/default.nix
@@ -1,4 +1,7 @@
-{ ... }:
+{ config, ... }:
 {
-  home.file.".npmrc".text = builtins.readFile ./npmrc;
+  xdg.configFile."npm/npmrc".text = builtins.readFile ./npmrc;
+  home.sessionVariables = {
+    NPM_CONFIG_USERCONFIG = "${config.xdg.configHome}/.config/npm/npmrc";
+  };
 }
