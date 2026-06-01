b68a931298e5891017cd500856676b2041004f74
Omochice <mochice.mls.ntl@gmail.com>
2026-06-01 10:04:03 +0900

    refactor: use llm-agents#ccusage instead

--- config/sketchybar/default.nix
+++ config/sketchybar/default.nix
@@ -1,4 +1,7 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
+let
+  llm-pkgs = inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system};
+in
 {
   xdg.configFile = {
     sketchybar = {
@@ -8,7 +11,8 @@
       text = ''
         #!/usr/bin/env bash
         set -ue
-        COST="$(${pkgs.ccusage}/bin/ccusage --offline --json | ${pkgs.jq}/bin/jq -r '{ cost: .totals.totalCost, daily: .daily[-1].totalCost } | map_values((. * 100 | ceil ) / 100) | "$\(.cost) ($\(.daily)/d)"')"
+        COST="$(${pkgs.lib.getExe llm-pkgs.ccusage} --offline --json \
+          | ${pkgs.lib.getExe pkgs.jq} -r '{ cost: .totals.totalCost, daily: .daily[-1].totalCost } | map_values((. * 100 | ceil ) / 100) | "$\(.cost) ($\(.daily)/d)"')"
         sketchybar -m --set $NAME label="$COST"
       '';
       executable = true;
