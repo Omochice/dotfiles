7db8a3d7c5acd0924b79c1885f7b00e0f214662d
Omochice <mochice.mls.ntl@gmail.com>
2026-09-11 01:01:51 +0900

    fix: use default [c|]c if wo.diff

--- config/nvim/rc/hooks/gitsigns.lua
+++ config/nvim/rc/hooks/gitsigns.lua
@@ -1,12 +1,22 @@
 -- lua_source {{{
 local gitsign = require("gitsigns")
+
+---@param direction "next"|"prev"
+---@param native string
+local function nav_hunk(direction, native)
+  return function()
+    if vim.wo.diff then
+      vim.cmd.normal({ native, bang = true })
+      return
+    end
+    gitsign.nav_hunk(direction)
+  end
+end
+
 gitsign.setup({
   on_attach = function(bufnr)
-    local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
-    if filetype ~= "diff" then
-      vim.keymap.set("n", "[c", gitsign.prev_hunk, { buffer = bufnr })
-      vim.keymap.set("n", "]c", gitsign.next_hunk, { buffer = bufnr })
-    end
+    vim.keymap.set("n", "[c", nav_hunk("prev", "[c"), { buffer = bufnr })
+    vim.keymap.set("n", "]c", nav_hunk("next", "]c"), { buffer = bufnr })
     vim.keymap.set("n", "<Space>gm", gitsign.blame_line, { buffer = bufnr })
   end,
   preview_config = {
